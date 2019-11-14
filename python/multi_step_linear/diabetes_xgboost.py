ad4b05d9225cc35310c754a94e8c1f2b44c129e8
Ketan Umare <kumare@lyft.com>
2019-11-13 16:25:28 -0800

    updated

--- python/multi_step_linear/diabetes_xgboost.py
+++ python/multi_step_linear/diabetes_xgboost.py
@@ -3,14 +3,13 @@ from __future__ import division
 from __future__ import print_function
 
 import joblib
+import pandas as pd
 from flytekit.sdk.tasks import python_task, outputs, inputs
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Output, Input
-from numpy import loadtxt
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
-import pandas as pd
 
 # Since we are working with a specific dataset, we will create a strictly typed schema for the dataset.
 # If we wanted a generic data splitter we could use a Generic schema without any column type and name information
@@ -26,7 +25,7 @@ import pandas as pd
 #  8. Age (years)
 #  9. Class variable (0 or 1)
 # Example Row: 6,148,72,35,0,33.6,0.627,50,1
-DATASET_SCHEMA = Types.Schema([
+TYPED_COLUMNS = [
     ('#preg', Types.Integer),
     ('pgc_2h', Types.Integer),
     ('diastolic_bp', Types.Integer),
@@ -36,79 +35,90 @@ DATASET_SCHEMA = Types.Schema([
     ('diabetes_pedigree', Types.Float),
     ('age', Types.Integer),
     ('class', Types.Integer),
-])
+]
+DATASET_SCHEMA = Types.Schema(TYPED_COLUMNS)
+FEATURES_SCHEMA = Types.Schema(TYPED_COLUMNS[:8])
+CLASSES_SCHEMA = Types.Schema([TYPED_COLUMNS[-1]])
+
 
 # load data
 # Example file: https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv
 @inputs(dataset=Types.CSV, seed=Types.Integer, test_split_ratio=Types.Float)
-@outputs(x_train=DATASET_SCHEMA, x_test=DATASET_SCHEMA, y_train=DATASET_SCHEMA, y_test=DATASET_SCHEMA)
-@python_task(cache_version='1.0',cache=True)
+@outputs(x_train=FEATURES_SCHEMA, x_test=FEATURES_SCHEMA, y_train=CLASSES_SCHEMA, y_test=CLASSES_SCHEMA)
+@python_task(cache_version='1.0', cache=True)
 def get_traintest_splitdatabase(ctx, dataset, seed, test_split_ratio, x_train, x_test, y_train, y_test):
-        dataset.download()
-        column_names = [k for k in DATASET_SCHEMA.columns.keys()]
-        df = pd.read_csv(dataset.local_path, names=column_names)
+    dataset.download()
+    column_names = [k for k in DATASET_SCHEMA.columns.keys()]
+    df = pd.read_csv(dataset.local_path, names=column_names)
 
-        # Select all features
-        x = df[column_names[:8]]
-        # Select only the classes
-        y = df[[column_names[-1]]]
+    # Select all features
+    x = df[column_names[:8]]
+    # Select only the classes
+    y = df[[column_names[-1]]]
 
-        # split data into train and test sets
-        _x_train,_x_test, _y_train, _y_test = train_test_split(
-                x, y, test_size=test_split_ratio, random_state=seed)
+    # split data into train and test sets
+    _x_train, _x_test, _y_train, _y_test = train_test_split(
+        x, y, test_size=test_split_ratio, random_state=seed)
 
-        def pd_df_to_schema(_df):
-            arr_schema = DATASET_SCHEMA()
-            with arr_schema as w:
-                print(_df.info())
-                w.write(_df)
-            return arr_schema
+    def pd_df_to_schema(_schema, _df):
+        arr_schema = _schema()
+        with arr_schema as w:
+            w.write(_df)
+        return arr_schema
 
-        #https: // github.com / lyft / flytekit / blob / master / flytekit / common / types / impl / schema.py  # L592
-        # Add support for ndarray and pd
-        x_train.set(pd_df_to_schema(_x_train))
-        x_test.set(pd_df_to_schema(_x_test))
-        y_train.set(pd_df_to_schema(_y_train))
-        y_test.set(pd_df_to_schema(_y_test))
+    # https: // github.com / lyft / flytekit / blob / master / flytekit / common / types / impl / schema.py  # L592
+    # Add support for pd, directly
+    x_train.set(pd_df_to_schema(FEATURES_SCHEMA, _x_train))
+    x_test.set(pd_df_to_schema(FEATURES_SCHEMA, _x_test))
+    y_train.set(pd_df_to_schema(CLASSES_SCHEMA, _y_train))
+    y_test.set(pd_df_to_schema(CLASSES_SCHEMA, _y_test))
 
 
-@inputs(x=DATASET_SCHEMA, y=DATASET_SCHEMA)
-@outputs(model=Types.Blob) # TODO: Support for subtype format=".joblib.dat"))
-@python_task(cache_version='1.0',cache=True)
+@inputs(x=FEATURES_SCHEMA, y=CLASSES_SCHEMA)
+@outputs(model=Types.Blob)  # TODO: Support for subtype format=".joblib.dat"))
+@python_task(cache_version='1.0', cache=True)
 def fit(ctx, x, y, model):
+    with x as r:
+        x_df = r.read()
+    with y as r:
+        y_df = r.read()
     # fit model no training data
     m = XGBClassifier()
-    m.fit(x, y)
+    m.fit(x_df, y_df)
     fname = "model.joblib.dat"
     joblib.dump(m, fname)
     model.set(fname)
 
 
-@inputs(x=DATASET_SCHEMA, model_ser=Types.Blob) #TODO: format=".joblib.dat"))
+@inputs(x=FEATURES_SCHEMA, model_ser=Types.Blob)  # TODO: format=".joblib.dat"))
 @outputs(predictions=[Types.Integer])
-@python_task(cache_version='1.0',cache=True)
+@python_task(cache_version='1.0', cache=True)
 def predict(ctx, x, model_ser, predictions):
     fname = "model.joblib.dat"
     model_ser.download(fname)
     model = joblib.load(fname)
     # make predictions for test data
-    y_pred = model.predict(x)
+    with x as r:
+        x_df = r.read()
+    y_pred = model.predict(x_df)
     predictions.set([round(value) for value in y_pred])
 
 
 @inputs(predictions=[Types.Integer], y_test=DATASET_SCHEMA)
 @outputs(accuracy=Types.Float)
-@python_task(cache_version='1.0',cache=True)
+@python_task(cache_version='1.0', cache=True)
 def score(ctx, predictions, y_test, accuracy):
     # evaluate predictions
-    accuracy = accuracy_score(y_test, predictions)
-    print("Accuracy: %.2f%%" % (accuracy * 100.0))
-    accuracy.set(accuracy)
+    acc = accuracy_score(y_test, predictions)
+    print("Accuracy: %.2f%%" % (acc * 100.0))
+    accuracy.set(acc)
 
-    
+
 @workflow_class
 class DiabetesXGBoostModelTrainer(object):
-    dataset = Input(Types.CSV, default=Types.CSV.create_at_known_location("https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"), help="A CSV File that matches the format https://github.com/jbrownlee/Datasets/blob/master/pima-indians-diabetes.names")
+    dataset = Input(Types.CSV, default=Types.CSV.create_at_known_location(
+        "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"),
+                    help="A CSV File that matches the format https://github.com/jbrownlee/Datasets/blob/master/pima-indians-diabetes.names")
 
     test_split_ratio = Input(Types.Float, default=0.33, help="Ratio of how much should be test to Train")
     seed = Input(Types.Integer, default=7, help="What should be the seed used for splitting")
--- python/tests/multi_step_linear/test_diabetes_xgboost.py
+++ python/tests/multi_step_linear/test_diabetes_xgboost.py
@@ -5,8 +5,24 @@ from multi_step_linear import diabetes_xgboost as dxgb
 
 
 @flyte_test
-def test_get_traintest_splitdatabase():
+def test_DiabetesXGBoostModelTrainer():
+
     dataset = Types.CSV.create_at_known_location(
         "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv")
+
+    # Test get dataset
     result = dxgb.get_traintest_splitdatabase.unit_test(dataset=dataset, seed=7, test_split_ratio=0.33)
-    print(result.x_train)
+    assert "x_train" in result
+    assert "y_train" in result
+    assert "x_test" in result
+    assert "y_test" in result
+
+    # Test fit
+    m = dxgb.fit.unit_test(x=result["x_train"], y=result["y_train"])
+
+    assert "model" in m
+
+    p = dxgb.predict.unit_test(x=result["x_test"], model_ser=m["model"])
+    assert "predictions" in p
+
+    print(p)
