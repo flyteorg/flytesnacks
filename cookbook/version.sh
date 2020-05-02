4fffbe68d0cb37c8b43e17fa214bbfb4a6ae416e
Yee Hing Tong <ytong@lyft.com>
2020-05-01 18:12:28 -0700

    wip

--- cookbook/version.sh
+++ cookbook/version.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-VERSION=""
-while IFS=':' read -ra IMG; do
-  for i in "${IMG[@]}"; do
-      VERSION=$i
-  done
-done <<< "${FLYTE_INTERNAL_IMAGE}"
-echo ${VERSION}
+echo $(git rev-parse HEAD)
