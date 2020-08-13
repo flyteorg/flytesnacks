61ea8f927969d56c80cbd3c82c1a1b62447f6754
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-13 09:39:45 -0700

    PR Comments

--- cookbook/recipes/workflows/advanced_flags.py
+++ cookbook/recipes/workflows/advanced_flags.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import print_function
-
-
 from flytekit.sdk.tasks import inputs
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Input, Output
