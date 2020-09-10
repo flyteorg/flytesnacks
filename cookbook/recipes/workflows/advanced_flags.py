61745e3b5b0c0a6f95b4a9f6c00f7399f3d2c4b6
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2020-09-10 13:14:49 -0700

    lyft-flyte presto update (#36)

--- cookbook/recipes/workflows/advanced_flags.py
+++ cookbook/recipes/workflows/advanced_flags.py
@@ -1,8 +1,7 @@
-from flytekit.sdk.tasks import inputs
+from flytekit.common.tasks.presto_task import SdkPrestoTask
+from flytekit.sdk.tasks import inputs, python_task
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Input, Output
-from flytekit.common.tasks.presto_task import SdkPrestoTask
-from flytekit.models.common import RawOutputDataConfig
 
 schema = Types.Schema([("a", Types.Integer), ("b", Types.String)])
 
@@ -16,15 +15,25 @@ presto_task = SdkPrestoTask(
 )
 
 
+@inputs(query_output=Types.Schema())
+@python_task
+def print_schemas(wf_params, query_output):
+    with query_output as r:
+        for x in r.iter_chunks():
+            df = x.read()
+            wf_params.logging.info(f"Output schema:\n{df}")
+
+
 @workflow_class()
-class CustomRawOutputWorkflow(object):
+class PrestoWorkflow(object):
     length = Input(Types.Integer, required=True, help="Int between 1 and 26")
     routing_group = Input(Types.String, required=True, help="Test string with no default")
     p_task = presto_task(length=length, rg=routing_group)
+    p = print_schemas(query_output=p_task.outputs.results)
     output_a = Output(p_task.outputs.results, sdk_type=schema)
 
 
 # Workflows can assign a default output location for raw data generated during execution.
-raw_output_lp = CustomRawOutputWorkflow.create_launch_plan(
-    raw_output_data_prefix='s3://test-raw-bucket/'
+raw_output_lp = PrestoWorkflow.create_launch_plan(
+    raw_output_data_prefix='s3://lyft-modelbuilder/'
 )
