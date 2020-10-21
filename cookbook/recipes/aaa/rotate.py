3caecbf8cd920bb426b7130eed06405a352bea86
Yee Hing Tong <ytong@lyft.com>
2020-10-21 13:34:39 -0700

    wip

--- cookbook/recipes/aaa/rotate.py
+++ cookbook/recipes/aaa/rotate.py
@@ -5,7 +5,7 @@ import cv2
 from flytekit import typing as flyte_typing
 from flytekit.annotated.task import task
 from flytekit.annotated.workflow import workflow
-from flytekit.common import utils
+import flytekit
 
 default_images = [
     'https://upload.wikimedia.org/wikipedia/commons/a/a8/Fractal_pyramid.jpg',
@@ -19,19 +19,19 @@ def rotate(image_location: str) -> os.PathLike:
     """
     Download the given image, rotate it by 180 degrees
     """
-    with utils.AutoDeletingTempDir('rotation') as tmp:
-        local_image = os.path.join(tmp.name, 'incoming.jpg')
-        urllib.request.urlretrieve(image_location, local_image)
-        img = cv2.imread(local_image, 0)
-        if img is None:
-            raise Exception("Failed to read image")
-        (h, w) = img.shape[:2]
-        center = (w / 2, h / 2)
-        mat = cv2.getRotationMatrix2D(center, 180, 1)
-        res = cv2.warpAffine(img, mat, (w, h))
-        out_path = os.path.join(tmp.name, "rotated.jpg")
-        cv2.imwrite(out_path, res)
-        return flyte_typing.FlyteFilePath(path=out_path)
+    working_dir = flytekit.current_context().working_dir
+    local_image = os.path.join(working_dir, 'incoming.jpg')
+    urllib.request.urlretrieve(image_location, local_image)
+    img = cv2.imread(local_image, 0)
+    if img is None:
+        raise Exception("Failed to read image")
+    (h, w) = img.shape[:2]
+    center = (w / 2, h / 2)
+    mat = cv2.getRotationMatrix2D(center, 180, 1)
+    res = cv2.warpAffine(img, mat, (w, h))
+    out_path = os.path.join(working_dir, "rotated.jpg")
+    cv2.imwrite(out_path, res)
+    return flyte_typing.FlyteFilePath(path=out_path)
 
 
 @workflow
