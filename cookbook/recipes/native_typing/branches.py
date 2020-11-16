49ba8f519ddcf57b20aac4cb85684afe6ef6efd2
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-11-16 10:30:06 -0800

    add branches example

---
+++ cookbook/recipes/native_typing/branches.py
@@ -0,0 +1,27 @@
+import typing
+
+from flytekit import conditional, task, workflow
+
+
+@task
+def t1(a: int) -> typing.NamedTuple("OutputsBC", t1_int_output=int, c=str):
+    return a + 2, "world"
+
+@task
+def t2(a: str) -> str:
+    return a
+
+@workflow
+def my_wf(a: int, b: str) -> (int, str):
+    x, y = t1(a=a)
+    d = (
+        conditional("test1")
+            .if_(x == 4)
+            .then(t2(a=b))
+            .elif_(x >= 5)
+            .then(t2(a=y))
+            .else_()
+            .fail("Unable to choose branch")
+    )
+    f = conditional("test2").if_(d == "hello").then(t2(a="It is hello")).else_().then(t2(a="Not Hello!"))
+    return x, f
