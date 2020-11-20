f8b199c0da92816457733224b486db443f5763d3
Yee Hing Tong <ytong@lyft.com>
2020-11-19 17:15:57 -0800

    adding first cut of second edition

---
+++ cookbook_se/recipes/native_typing/raw_container.py
@@ -0,0 +1,43 @@
+from flytekit import ContainerTask, kwtypes, metadata, workflow
+
+square = ContainerTask(
+    name="square",
+    metadata=metadata(),
+    input_data_dir="/var/inputs",
+    output_data_dir="/var/outputs",
+    inputs=kwtypes(val=int),
+    outputs=kwtypes(out=int),
+    image="alpine",
+    command=[
+        "sh",
+        "-c",
+        "echo $(( {{.Inputs.val}} * {{.Inputs.val}} )) | tee /var/outputs/out",
+    ],
+)
+
+sum = ContainerTask(
+    name="sum",
+    metadata=metadata(),
+    input_data_dir="/var/flyte/inputs",
+    output_data_dir="/var/flyte/outputs",
+    inputs=kwtypes(x=int, y=int),
+    outputs=kwtypes(out=int),
+    image="alpine",
+    command=[
+        "sh",
+        "-c",
+        "echo $(( {{.Inputs.x}} + {{.Inputs.y}} )) | tee /var/flyte/outputs/out",
+    ],
+)
+
+
+@workflow
+def raw_container_wf(val1: int, val2: int) -> int:
+    return sum(x=square(val=val1), y=square(val=val2))
+
+
+if __name__ == "__main__":
+    print(f"Running {__file__} main...")
+    print(
+        f"Running raw_container_wf(val1=5, val2=5) {raw_container_wf(val1=5, val2=5)}"
+    )
