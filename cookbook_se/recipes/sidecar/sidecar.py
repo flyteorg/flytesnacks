874d073be907a8c4059353aa753758a06eb977a2
Katrina Rogan <krogan@lyft.com>
2020-12-02 15:57:09 -0800

    imports

--- cookbook_se/recipes/sidecar/sidecar.py
+++ cookbook_se/recipes/sidecar/sidecar.py
@@ -1,13 +1,12 @@
-import time
 import os
+import time
 
 from flytekit import task, workflow
 from flytekit.taskplugins.sidecar import Sidecar
-
 from k8s.io.api.core.v1 import generated_pb2
 from k8s.io.apimachinery.pkg.api.resource.generated_pb2 import Quantity
 
-_SHARED_DATA_PATH = '/data/message.txt'
+_SHARED_DATA_PATH = "/data/message.txt"
 
 
 # A simple pod spec in which a shared volume is mounted in both the primary and secondary containers. The secondary
@@ -17,12 +16,11 @@ def generate_pod_spec_for_task():
 
     primary_container = generated_pb2.Container(name="primary")
 
-    secondary_container = generated_pb2.Container(
-              name="secondary",
-              image="alpine",
-    )
+    secondary_container = generated_pb2.Container(name="secondary", image="alpine",)
     secondary_container.command.extend(["/bin/sh"])
-    secondary_container.args.extend(["-c", "echo hi sidecar world > {}".format(_SHARED_DATA_PATH)])
+    secondary_container.args.extend(
+        ["-c", "echo hi sidecar world > {}".format(_SHARED_DATA_PATH)]
+    )
 
     resources = generated_pb2.ResourceRequirements()
     resources.limits["cpu"].CopyFrom(Quantity(string="1"))
@@ -33,31 +31,36 @@ def generate_pod_spec_for_task():
     secondary_container.resources.CopyFrom(resources)
 
     shared_volume_mount = generated_pb2.VolumeMount(
-              name="shared-data",
-              mountPath="/data",
+        name="shared-data", mountPath="/data",
     )
     secondary_container.volumeMounts.extend([shared_volume_mount])
     primary_container.volumeMounts.extend([shared_volume_mount])
 
-    pod_spec.volumes.extend([generated_pb2.Volume(
-               name="shared-data",
-               volumeSource=generated_pb2.VolumeSource(
-                   emptyDir=generated_pb2.EmptyDirVolumeSource(
-                              medium="Memory",
-                   )
-               )
-    )])
+    pod_spec.volumes.extend(
+        [
+            generated_pb2.Volume(
+                name="shared-data",
+                volumeSource=generated_pb2.VolumeSource(
+                    emptyDir=generated_pb2.EmptyDirVolumeSource(medium="Memory",)
+                ),
+            )
+        ]
+    )
     pod_spec.containers.extend([primary_container, secondary_container])
     return pod_spec
 
 
-@task(task_config=Sidecar(pod_spec=generate_pod_spec_for_task(), primary_container_name="primary"))
+@task(
+    task_config=Sidecar(
+        pod_spec=generate_pod_spec_for_task(), primary_container_name="primary"
+    )
+)
 def my_sidecar_task() -> str:
     # The code defined in this task will get injected into the primary container.
     while not os.path.isfile(_SHARED_DATA_PATH):
         time.sleep(5)
 
-    with open(_SHARED_DATA_PATH, 'r') as shared_message_file:
+    with open(_SHARED_DATA_PATH, "r") as shared_message_file:
         return shared_message_file.read()
 
 
