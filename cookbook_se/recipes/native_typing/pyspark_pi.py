ce9b97ca7faa64b8cec6dd45b384a4636415e836
Yee Hing Tong <ytong@lyft.com>
2020-12-02 17:19:26 -0800

    wip

--- cookbook_se/recipes/native_typing/pyspark_pi.py
+++ cookbook_se/recipes/native_typing/pyspark_pi.py
@@ -1,3 +1,4 @@
+import flytekit
 import random
 import datetime
 from operator import add
@@ -15,11 +16,12 @@ from flytekit.taskplugins.spark import Spark
     }),
     cache_version='1',
     container_image='{{.image.default.fqn}}:spark-{{.image.default.version}}')
-def hello_spark(spark_context, partitions: int) -> float:
+def hello_spark(partitions: int) -> float:
     print("Starting Spark with Partitions: {}".format(partitions))
 
     n = 100000 * partitions
-    count = spark_context.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    sess = flytekit.current_context().spark_session
+    count = sess.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
     pi_val = 4.0 * count / n
     print("Pi val is :{}".format(pi_val))
     return pi_val
