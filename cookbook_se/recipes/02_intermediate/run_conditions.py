ef2a696ffa9b7dc86973d7ffd1bce26adefaf7a5
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-01-06 10:03:49 -0800

    temp commenting conditions

--- cookbook_se/recipes/02_intermediate/run_conditions.py
+++ cookbook_se/recipes/02_intermediate/run_conditions.py
@@ -49,61 +49,61 @@ def double(n: float) -> float:
     return 2 * n
 
 
-@workflow
-def multiplier(my_input: float) -> float:
-    return (
-        conditional("fractions")
-        .if_((my_input >= 0.1) & (my_input <= 1.0))
-        .then(double(n=my_input))
-        .else_()
-        .then(square(n=my_input))
-    )
-
-
-print(f"Output of multiplier(my_input=3): {multiplier(my_input=3)}")
-print(f"Output of multiplier(my_input=0.5): {multiplier(my_input=0.5)}")
-
-
-# %%
-# Example 2
-# ^^^^^^^^^
-# In the following example we have an if condition with multiple branches and we fail if no conditions are met. Flyte
-# expects any conditional() statement to be _complete_ meaning all possible branches have to be handled.
-@workflow
-def multiplier_2(my_input: float) -> float:
-    return (
-        conditional("fractions")
-        .if_((my_input > 0.1) & (my_input < 1.0))
-        .then(double(n=my_input))
-        .elif_((my_input > 1.0) & (my_input <= 10.0))
-        .then(square(n=my_input))
-        .else_()
-        .fail("The input must be between 0 and 10")
-    )
-
-
-print(f"Output of multiplier_2(my_input=10): {multiplier_2(my_input=10)}")
-
-
-# %%
-# Example 3
-# ^^^^^^^^^
-# In the following example we consume the output returned by the conditional() in a subsequent task.
-@workflow
-def multiplier_3(my_input: float) -> float:
-    d = (
-        conditional("fractions")
-        .if_((my_input > 0.1) & (my_input < 1.0))
-        .then(double(n=my_input))
-        .elif_((my_input > 1.0) & (my_input < 10.0))
-        .then(square(n=my_input))
-        .else_()
-        .fail("The input must be between 0 and 10")
-    )
-
-    # d will be either the output of `double` or t he output of `square`. If the conditional() falls through the fail
-    # branch, execution will not reach here.
-    return double(n=d)
-
-
-print(f"Output of multiplier_3(my_input=5): {multiplier_3(my_input=5)}")
+# @workflow
+# def multiplier(my_input: float) -> float:
+#     return (
+#         conditional("fractions")
+#         .if_((my_input >= 0.1) & (my_input <= 1.0))
+#         .then(double(n=my_input))
+#         .else_()
+#         .then(square(n=my_input))
+#     )
+#
+#
+# print(f"Output of multiplier(my_input=3): {multiplier(my_input=3)}")
+# print(f"Output of multiplier(my_input=0.5): {multiplier(my_input=0.5)}")
+#
+#
+# # %%
+# # Example 2
+# # ^^^^^^^^^
+# # In the following example we have an if condition with multiple branches and we fail if no conditions are met. Flyte
+# # expects any conditional() statement to be _complete_ meaning all possible branches have to be handled.
+# @workflow
+# def multiplier_2(my_input: float) -> float:
+#     return (
+#         conditional("fractions")
+#         .if_((my_input > 0.1) & (my_input < 1.0))
+#         .then(double(n=my_input))
+#         .elif_((my_input > 1.0) & (my_input <= 10.0))
+#         .then(square(n=my_input))
+#         .else_()
+#         .fail("The input must be between 0 and 10")
+#     )
+#
+#
+# print(f"Output of multiplier_2(my_input=10): {multiplier_2(my_input=10)}")
+#
+#
+# # %%
+# # Example 3
+# # ^^^^^^^^^
+# # In the following example we consume the output returned by the conditional() in a subsequent task.
+# @workflow
+# def multiplier_3(my_input: float) -> float:
+#     d = (
+#         conditional("fractions")
+#         .if_((my_input > 0.1) & (my_input < 1.0))
+#         .then(double(n=my_input))
+#         .elif_((my_input > 1.0) & (my_input < 10.0))
+#         .then(square(n=my_input))
+#         .else_()
+#         .fail("The input must be between 0 and 10")
+#     )
+#
+#     # d will be either the output of `double` or t he output of `square`. If the conditional() falls through the fail
+#     # branch, execution will not reach here.
+#     return double(n=d)
+#
+#
+# print(f"Output of multiplier_3(my_input=5): {multiplier_3(my_input=5)}")
