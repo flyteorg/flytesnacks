9bfc3c386f74bb6151af7f51d07f8772553e42e5
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-01-07 14:37:07 -0800

    wip

--- cookbook_se/recipes/demo/work.py
+++ cookbook_se/recipes/demo/work.py
@@ -1,18 +1,23 @@
 from typing import List, Tuple
+from flytekit import task
 
 
 def tower(n, source, destination, auxiliary) -> List[Tuple[int, int]]:
     if n == 1:
         return [(source, destination)]
+
     results = tower(n - 1, source, auxiliary, destination)
     results.append((source, destination))
     results.extend(tower(n - 1, auxiliary, destination, source))
     return results
 
 
-def solve_tower(num_discs: int):
-    print(tower(num_discs, 1, 3, 2))
+@task
+def solve_tower(num_discs: int) -> int:
+    results = tower(num_discs, 1, 3, 2)
+    print(results)
+    return len(results)
 
 
 if __name__ == "__main__":
-    solve_tower(3)
+    print(solve_tower(num_discs=3))
