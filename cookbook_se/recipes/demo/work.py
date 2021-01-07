b09c6a14f0162d32f3d7af7a76baef624c93cd1a
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-01-07 13:54:00 -0800

    workflow

--- cookbook_se/recipes/demo/work.py
+++ cookbook_se/recipes/demo/work.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
-from flytekit import task, Resources
+import pandas as pd
+from flytekit import task, Resources, workflow
+from flytekit.types.schema import FlyteSchema
 
 
 def tower(n, source, destination, auxiliary) -> List[Tuple[int, int]]:
@@ -13,10 +15,23 @@ def tower(n, source, destination, auxiliary) -> List[Tuple[int, int]]:
 
 
 @task(requests=Resources(cpu='1', mem="2Gi"), limits=Resources(cpu='1', mem="2Gi"))
-def solve_tower(num_discs: int) -> int:
+def solve_tower(num_discs: int) -> pd.DataFrame:
     results = tower(num_discs, 1, 3, 2)
-    return len(results)
+    df = pd.DataFrame.from_records(results, columns=['From', 'To'])
+    return df
+
+
+@task(requests=Resources(cpu='1', mem="2Gi"), limits=Resources(cpu='1', mem="2Gi"))
+def count_rows(steps: pd.DataFrame) -> int:
+    # Reconstitute the DataFrame
+    return len(steps.index)
+
+
+@workflow
+def tower_wf(discs: int) -> int:
+    step_schema = solve_tower(num_discs=discs)
+    return count_rows(steps=step_schema)
 
 
 if __name__ == "__main__":
-    print(solve_tower(num_discs=3))
+    print(tower_wf(discs=3))
