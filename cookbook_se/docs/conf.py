b904076125c88868dfc356d64847e0e6f4d85cbf
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-01-08 17:31:34 -0800

    explicit order

--- cookbook_se/docs/conf.py
+++ cookbook_se/docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 from sphinx_gallery.sorting import ExampleTitleSortKey
+from sphinx_gallery.sorting import ExplicitOrder
 
 sys.path.insert(0, os.path.abspath("../"))
 
@@ -93,6 +94,13 @@ min_reported_time = 0
 sphinx_gallery_conf = {
     "examples_dirs": examples_dirs,
     "gallery_dirs": gallery_dirs,
+    "subsection_order": ExplicitOrder(['../recipes/02_intermediate',
+                                       '../recipes/03_advanced',
+                                       '../recipes/01_basic',
+                                       '../recipes/05_native_plugins',
+                                       '../recipes/04_remote_flyte',
+                                       '../recipes/06_aws_plugins',
+                                       ]),
     # specify the order of examples to be according to filename
     "within_subsection_order": ExampleTitleSortKey,
     "min_reported_time": min_reported_time,
