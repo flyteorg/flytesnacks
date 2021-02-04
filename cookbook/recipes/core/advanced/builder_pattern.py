406e6cb880f23e3735b73adcabfdfbf8c9249fc0
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-02-04 09:17:55 -0800

    wip

---
+++ cookbook/recipes/core/advanced/builder_pattern.py
@@ -0,0 +1,47 @@
+from flytekit import task, workflow
+
+
+@task
+def tsk_write(in1: str):
+    print(f"Write task {in1}")
+
+
+class Builder:
+    def __init__(self, query: str):
+        self.do_fn = None
+        self.query = query
+
+    @staticmethod
+    def create(query: str = None):
+        return Builder(query)
+
+    def assign_function(self, do_fn):
+        self.do_fn = do_fn
+        return self
+
+    def build(self):
+        @task
+        def my_task() -> str:
+            print(f"In nested task, query is {self.query}")
+            self.do_fn()
+            return "hello from the inner function"
+
+        @workflow
+        def my_wf() -> None:
+            inner_task_output = my_task()
+            tsk_write(in1=inner_task_output)
+
+        return my_wf, my_task
+
+
+def transform():
+    print("In transform")
+
+
+sample_query = "select * from my_input_table"
+built_wf, built_task = Builder.create(sample_query).assign_function(transform).build()
+
+
+if __name__ == "__main__":
+    built_wf()
+    print(f"WF {built_wf}, Task {built_task}")
