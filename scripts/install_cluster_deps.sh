68bd26d9372d105e29e6f1fbb593aa8bac90dd74
Jeev B <jeev.balakrishnan@freenome.com>
2021-02-12 16:00:01 -0800

    add k3c

--- scripts/install_cluster_deps.sh
+++ scripts/install_cluster_deps.sh
@@ -9,7 +9,16 @@ get_k3d() {
     local executable="${target_dir}/k3d"
     local version="${K3D_VERSION:-v4.2.0}"
 
-    ( [ -f ${executable} ] && $(${executable} --version | grep -Fxq "k3d version ${version}") ) || curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | USE_SUDO=false K3D_INSTALL_DIR=${target_dir} TAG="${version}" bash > /dev/null
+    ( [ -f ${executable} ] && $(${executable} --version | grep -q "k3d version ${version}") ) || curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | USE_SUDO=false K3D_INSTALL_DIR=${target_dir} TAG="${version}" bash > /dev/null
+}
+
+get_k3c() {
+    local target_dir="$1"
+    local executable="${target_dir}/k3c"
+    local version="${K3C_VERSION:-v0.3.0-alpha.0}"
+
+    # TODO: determine arm vs amd64 architecture
+    ( [ -f ${executable} ] && $(${executable} --version | grep -q "k3c version ${version}") ) || ( curl -L -o ${executable} https://github.com/rancher/k3c/releases/download/${version}/k3c-${PLATFORM}-amd64 && chmod +x ${executable} )
 }
 
 get_kubectl() {
@@ -20,6 +29,6 @@ get_kubectl() {
 }
 
 mkdir -p _bin
-for dep in k3d kubectl; do
+for dep in k3d k3c kubectl; do
     "get_${dep}" _bin
 done
