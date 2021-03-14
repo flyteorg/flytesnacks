b2ce9082918d3216ce9c1feb0c09bb1f928c8eff
Ketan Umare <ketan.umare@gmail.com>
2021-03-14 14:06:22 -0700

    Updated example
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- cookbook/core/intermediate/use_secrets.py
+++ cookbook/core/intermediate/use_secrets.py
@@ -31,7 +31,7 @@ SECRET_NAME = "user_secret"
 # .. note::
 #
 #   In case of failure to access the secret (it is not found at execution time) an error is raised
-@task(secret_requests=[Secret(name=SECRET_NAME)])
+@task(secret_requests=[Secret(key=SECRET_NAME)])
 def secret_task() -> str:
     secret_val = flytekit.current_context().secrets.get(SECRET_NAME)
     # Please do not print the secret value, we are doing so just as a demonstration
@@ -40,10 +40,37 @@ def secret_task() -> str:
 
 
 # %%
+# In some cases you may have multiple secrets and sometimes, they maybe grouped as one secret in the SecretStore.
+# For example, AWS Secret Manager, has the secret(name, version). Thus in this case the name would be the group, version
+# would be the key
+# In Kubernetes secrets, it is possible to nest multiple keys under the same secret. Thus in this case the name
+# would be the actual name of the nested secret, and the group would be the identifier for the kubernetes secret.
+#
+# As an example, let us define 2 secrets username and password, defined in the group user_info
+USERNAME_SECRET = "username"
+PASSWORD_SECRET = "password"
+SECRET_GROUP = "user_info"
+
+
+# %%
+# The Secret structure allows passing two fields, as described previously matching the key and the group
+@task(
+    secret_requests=[Secret(key=USERNAME_SECRET, group=SECRET_GROUP), Secret(key=PASSWORD_SECRET, group=SECRET_GROUP)])
+def user_info_task() -> (str, str):
+    secret_username = flytekit.current_context().secrets.get(USERNAME_SECRET, secrets_group=SECRET_GROUP)
+    secret_pwd = flytekit.current_context().secrets.get(PASSWORD_SECRET, secrets_group=SECRET_GROUP)
+    # Please do not print the secret value, we are doing so just as a demonstration
+    print(f"{secret_username}={secret_pwd}")
+    return secret_username, secret_pwd
+
+
+# %%
 # You can use these tasks in your workflow as usual
 @workflow
-def my_secret_workflow() -> str:
-    return secret_task()
+def my_secret_workflow() -> (str, str, str):
+    x = secret_task()
+    y, z = user_info_task()
+    return x, y, z
 
 
 # %%
@@ -54,4 +81,9 @@ from flytekit.testing import SecretsManager
 if __name__ == "__main__":
     sec = SecretsManager()
     os.environ[sec.get_secrets_env_var(SECRET_NAME)] = "value"
-    assert my_secret_workflow() == "value"
+    os.environ[sec.get_secrets_env_var(USERNAME_SECRET, SECRET_GROUP)] = "username_value"
+    os.environ[sec.get_secrets_env_var(PASSWORD_SECRET, SECRET_GROUP)] = "password_value"
+    x, y, z = my_secret_workflow()
+    assert x == "value"
+    assert y == "username_value"
+    assert z == "password_value"
