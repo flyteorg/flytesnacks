51db6eb713bcdcbb10157bb2d30c2e8a57a8e21a
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-04-02 15:05:36 -0700

    wip
    Signed-off-by: wild-endeavor <wild-endeavor@users.noreply.github.com>

---
+++ cookbook/core/intermediate/sql.py
@@ -0,0 +1,30 @@
+import pandas
+
+from flytekit import kwtypes, task, workflow
+from flytekit.extras.sqlite3.task import SQLite3Config, SQLite3Task
+
+# https://www.sqlitetutorial.net/sqlite-sample-database/
+from flytekit.types.schema import FlyteSchema
+
+EXAMPLE_DB = "https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip"
+
+sql_task = SQLite3Task(
+    "cookbook.sqlite3.sample",
+    query_template="select TrackId, Name from tracks limit {{.inputs.limit}}",
+    inputs=kwtypes(limit=int),
+    output_schema_type=FlyteSchema[kwtypes(TrackId=int, Name=str)],
+    task_config=SQLite3Config(
+        uri=EXAMPLE_DB,
+        compressed=True,
+    ),
+)
+
+
+@task
+def print_and_count_columns(df: pandas.DataFrame) -> int:
+    return len(df[df.columns[0]])
+
+
+@workflow
+def wf() -> int:
+    return print_and_count_columns(df=sql_task(limit=100))
