6e22e68b142d6f0f3e93f69869d96023b0e04006
SandraGH5 <80421934+SandraGH5@users.noreply.github.com>
2021-04-28 16:51:56 -0700

    Update customizing_resources.py

--- cookbook/core/remote_flyte/customizing_resources.py
+++ cookbook/core/remote_flyte/customizing_resources.py
@@ -1,21 +1,20 @@
 """
-Customizing task resources like mem/cpu
+Customizing Task Resources like mem/cpu
 --------------------------------------------
 
-One of the reasons why you would want to use a hosted Flyte environment is due to the potential of leveraging CPU,
-memory and storage resources, far greater than what's available locally.
+One of the reasons to use a hosted Flyte environment is the potential of leveraging CPU, memory and storage resources, far greater than what's available locally.
 Flytekit makes it possible to specify these requirements declaratively and close to where the task itself is declared.
 
 """
 
 # %%
-# In this example the memory required by the function increases as the dataset size increases.
-# For large datasets we may not be able to run locally. Thus we want to provide hints to flyte backend that we want to request for more memory.
+# In this example, the memory required by the function increases as the dataset size increases.
+# Large datasets may not be able to run locally, so we would want to provide hints to flyte backend to request for more memory.
 # This is done by simply decorating the task with the hints as shown in the following code sample.
 #
 # Tasks can have ``requests`` and ``limits`` which mirror the native `equivalents in kubernetes <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#requests-and-limits>`__
 # A task can possibly be allocated more resources than it requests, but never more than its limit.
-# Requests are treated as hints which are used to schedule your tasks on nodes with available resources, whereas limits
+# Requests are treated as hints which are used to schedule tasks on nodes with available resources, whereas limits
 # are hard constraints.
 #
 # For either a request or limit, refer to the :py:class:`flytekit:flytekit.Resource` documentation.
@@ -26,7 +25,11 @@ Flytekit makes it possible to specify these requirements declaratively and close
 # #. ``mem``
 # #. ``gpu``
 #
-# The ``storage`` resources option is not yet supported, but coming soon
+# It is not necessary to adjust admission controllers for gpu nodes, but they will need to be configured with a taint. 
+#
+# To ensure that the regular tasks do not get shceduled on a gpu node, set up flytepropeller using the following `configuration <https://github.com/flyteorg/flytepropeller/blob/master/config.yaml#L51,L56>`_.
+#
+# The ``storage`` resources option is not yet supported, but coming soon.
 #
 # The acutal values follow the `kubernetes convention <https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#resource-units-in-kubernetes>`_.
 
