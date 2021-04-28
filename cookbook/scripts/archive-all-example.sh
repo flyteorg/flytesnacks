0e723ee91ad0a3923cd3a5fae3c52ecfaa2a33ce
yuvraj <evalsocket@gmail.com>
2021-04-28 15:55:11 +0530

    More changes
    Signed-off-by: yuvraj <evalsocket@gmail.com>

--- cookbook/scripts/archive-all-example.sh
+++ cookbook/scripts/archive-all-example.sh
@@ -3,5 +3,5 @@
 set -e
 
 for row in $(cat flyte_tests_manifest.json | jq -c '.[]'); do
-  echo "tar -zcvf ./release-snacks/flytesnacks-$(echo ${row} | jq -r '.name').tar.gz  ./$(echo ${row} | jq -r '.path')/_pb_output"
+  tar -cvzf "./release-snacks/flytesnacks-$(echo ${row} | jq -r '.name').tgz"  "./$(echo ${row} | jq -r '.path')/_pb_output/"
 done
