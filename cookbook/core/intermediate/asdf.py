5e3451f6ed615bed2f39c83333ed38c5a12384a9
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-04-28 16:45:20 -0700

    wip
    Signed-off-by: wild-endeavor <wild-endeavor@users.noreply.github.com>

--- cookbook/core/intermediate/asdf.py
+++ cookbook/core/intermediate/asdf.py
@@ -1,21 +1,17 @@
 from flytekit import task, workflow
-import typing
-
-nt = typing.NamedTuple("SingleNamedOutput", named1=int)
 
 
 @task
-def t1(a: int) -> nt:
+def t1(a: int) -> int:
     a = a + 2
-    return (a,)
+    return a
 
 
 @workflow
-def subwf(a: int) -> nt:
+def subwf(a: int) -> int:
     return t1(a=a)
 
 
 @workflow
-def wf(b: int) -> nt:
-    out = subwf(a=b)
-    return t1(a=out.named1)
+def wf(b: int) -> int:
+    return subwf(a=b)
