4935167693950afd99bdd5f2928c4d85cbfa7b94
Niels Bantilan <niels.bantilan@gmail.com>
2021-05-04 09:15:09 -0400

    Update lp.py (#183)
    "How to add launch plans" page has been integrated into Launch Plans.
    
    Co-authored-by: SandraGH5 <80421934+SandraGH5@users.noreply.github.com>

--- cookbook/core/flyte_basics/lp.py
+++ cookbook/core/flyte_basics/lp.py
@@ -2,9 +2,9 @@
 .. _launch_plans:
 
 Launch Plans
-----------------------
+-------------
 
-Launch plans bind a partial or complete list of inputs necessary to launch a workflow along
+Launch plans bind a partial or complete list of inputs necessary to launch a workflow, along
 with optional run-time overrides such as notifications, schedules and more.
 Launch plan inputs must only assign inputs already defined in the reference workflow definition.
 """
@@ -12,10 +12,20 @@ Launch plan inputs must only assign inputs already defined in the reference work
 import calendar
 
 # %%
+# When To Use Launch Plans
+# ########################
+#
+# - For multiple schedules of a workflow with zero or more predefined inputs
+# - To run a specific workflow but with a different set of notifications
+# - To share a workflow with set inputs with another user, allowing the other user to simply kick off an execution
+# - To share a workflow with another user, making sure that some inputs can be overridden if needed
+# - To share a workflow with another user, ensuring that some inputs are not changed
+#
 # Launch plans are the only means for invoking workflow executions.
-# By default, a 'default' launch plan will be created during the serialization (and registration process),
+# A 'default' launch plan will be created during the serialization (and registration process),
 # which will optionally bind any default workflow inputs and any default runtime options specified in the project
 # flytekit config (such as user role, etc).
+#
 # The following example creates a default launch plan with no inputs during serialization.
 import datetime
 
@@ -43,19 +53,19 @@ square_4 = my_lp()
 square_5 = my_lp(val=5)
 
 # %%
-# In some cases you may want to **fix** launch plan inputs, such that they can't be overridden at execution call time.
+# It is possible to **fix** launch plan inputs, so that they can't be overridden at execution call time.
 my_fixed_lp = LaunchPlan.create("always_2_lp", my_wf, fixed_inputs={"val": 4})
 square_2 = my_fixed_lp()
 # error:
 # square_1 = my_fixed_lp(val=1)
 
 # %%
-# Putting it all together
+# Putting It All Together
 # #######################
 #
-# Default and fixed inputs can all be used in combination together to simplify individual executions
-# or even programmatic ones.
-# Let's take a look at a trivial example where we enthusiastically greet each day of the upcoming week:
+# Default and fixed inputs can be used together to simplify individual executions
+# and programmatic ones.
+# Here is a simple example to greet each day of the upcoming week:
 
 
 @task
@@ -87,3 +97,62 @@ for n in range(7):
     else:
         # We're extra enthusiastic on weekends
         print(morning_greeting(number=3, day_of_week=weekday))
+
+# %%
+# Partial Inputs for Launch Plans
+# ###############################
+# Launch plans bind a partial or complete list of inputs necessary to launch a workflow. Launch plan inputs must only assign inputs already defined in the reference workflow definition.  Refer to to :ref:`primary launch plan concept documentation <concepts-launchplans-inputs>` for a detailed introduction to launch plan input types.
+#
+# For example, for the following workflow definition:
+
+.. code:: python
+
+    from datetime import datetime
+    from flytekit import workflow
+
+    @workflow
+    def MyWorkflow(region: str, run_date: datetime, sample_size: int=100):
+        ...
+
+# To run the workflow over a set of date ranges for a specific region, you could define the following launch plan:
+
+.. code:: python
+
+    from flytekit import LaunchPlan
+
+    sea_launch_plan = LaunchPlan.create(
+        "sea_lp",
+        MyWorkflow,
+        default_inputs={'sample_size': 1000},
+        fixed_inputs={'region': 'SEA'},                
+    )
+
+# Some things to note here:
+# 
+# - The ``sample_size`` input is redefined in the launch plan - which is perfectly fine
+# - Workflow inputs with default values can be redefined in launch plans
+# - ``sample_size`` can also be adjusted at execution creation time because it is possible to override default_inputs in a launch plan
+# - It is not possible to override the region input as it is *fixed* 
+
+# The launch plan does not assign run_date, but it is possible to create a launch plan that assigns all workflow inputs (either as default or fixed inputs). The only requirement is that all required inputs 
+# without default values must be resolved at execution time. The call to create an execution can still accept inputs
+# should your launch plan not define the complete set.
+
+# Backfills With Launch Plans
+# ###########################
+
+# This is an example of how to use the launch plan to create executions programmatically to backfill:
+
+.. code:: python
+
+     from datetime import timedelta, date
+
+     run_date = date(2020, 1, 1)
+     end_date = date(2021, 1, 1)
+     one_day = timedelta(days=1)
+     while run_date < end_date:
+         sea_launch_plan(run_date=run_date)
+         run_date += one_day
+
+# Congratulations! A year's worth of executions have just been created!        
+        
