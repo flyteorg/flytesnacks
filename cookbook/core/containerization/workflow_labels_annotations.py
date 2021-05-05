7958c8c2f36d324f67bfcabc7d37a26ddb1b4769
SandraGH5 <80421934+SandraGH5@users.noreply.github.com>
2021-05-05 14:38:36 -0700

    Update workflow_labels_annotations.py

--- cookbook/core/containerization/workflow_labels_annotations.py
+++ cookbook/core/containerization/workflow_labels_annotations.py
@@ -1,8 +1,56 @@
 """
 Adding Workflow Labels and Annotations
----------------------------------------
+--------------------------------------
 
-.. NOTE::
+In Flyte, workflow executions are created as kubernetes resources. These can be extended with
+`labels <https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/>`_  and
+`annotations <https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/>`_.
 
-    Coming soon 🛠
+**Labels** and **annotations** are key value pairs which can be used to identify workflows for your own uses.
+Labels are meant to be used as identifing attributes, whereas annotations are arbitrary, *non-identifying* metadata.
+
+Using labels and annotations is entirely optional. They can be used to categorize and identify workflow executions.
+
+Labels and annotations are optional parameters to launch plan and execution invocations. When an execution
+defines labels and/or annotations *and* the launch plan does as well, the execution spec values will be preferred.
+
+Launch Plan Usage Example
+#########################
+
+.. code:: python
+
+    from flytekit.models.common import Labels, Annotations
+
+    @workflow
+    class MyWorkflow(object):
+        ...
+
+    my_launch_plan = MyWorkflow.create_launch_plan(
+        labels=Labels({"myexecutionlabel": "bar", ...}),
+        annotations=Annotations({"region": "SEA", ...}),
+        ...
+    )
+
+    my_launch_plan.execute(...)
+
+Execution Example
+#################
+
+.. code:: python
+
+    from flytekit.models.common import Labels, Annotations
+
+    @workflow
+    class MyWorkflow(object):
+        ...
+
+    my_launch_plan = MyWorkflow.create_launch_plan(...)
+
+    my_launch_plan.execute(
+        labels=Labels({"myexecutionlabel": "bar", ...}),
+        annotations=Annotations({"region": "SEA", ...}),
+        ...
+    )
+
+
 """
