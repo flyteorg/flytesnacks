e22ca8c2d307adf91909bd63a35f7d6a60a12a04
SandraGH5 <80421934+SandraGH5@users.noreply.github.com>
2021-05-07 14:06:51 -0700

    Update cookbook/core/containerization/workflow_labels_annotations.py
    Co-authored-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- cookbook/core/containerization/workflow_labels_annotations.py
+++ cookbook/core/containerization/workflow_labels_annotations.py
@@ -3,7 +3,7 @@ Adding Workflow Labels and Annotations
 --------------------------------------
 
 In Flyte, workflow executions are created as Kubernetes resources. These can be extended with
-`labels <https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/>`_  and
+`labels <https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/>`__ and
 `annotations <https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/>`_.
 
 **Labels** and **annotations** are key value pairs which can be used to identify workflows for your own uses.
