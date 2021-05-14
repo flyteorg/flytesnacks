96e15411c675e62295b462aee05bffa30042561a
Samhita Alla <aallasamhita@gmail.com>
2021-05-14 16:51:56 +0530

    rephrase the doc
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- cookbook/core/containerization/spot_instances.py
+++ cookbook/core/containerization/spot_instances.py
@@ -6,13 +6,13 @@ Using Spot/Preemptible Instances
 # %%
 # What Are Spot/Preemptible Instances?
 # ====================================
-# Spot instances are unused EC2 capacity in AWS. `Spot instances <https://aws.amazon.com/ec2/spot/?cards.sort-by=item.additionalFields.startDateTime&cards.sort-order=asc>`_ can result in up to 90% savings on on-demand prices. The caveat is that at any point these instances can be preempted and no longer be available for use. This can happen due to:
+# Spot instances are unused EC2 capacity in AWS. `Spot instances <https://aws.amazon.com/ec2/spot/?cards.sort-by=item.additionalFields.startDateTime&cards.sort-order=asc>`_ can result in up to 90% savings on on-demand prices. The caveat is that these instances can be preempted at any point and no longer be available for use. This can happen due to:
 #
 # * Price – The spot price is greater than your maximum price.
-# * Capacity – If there are not enough unused EC2 instances to meet the demand for spot instances, Amazon EC2 interrupts spot instances. The order in which the instances are interrupted is determined by Amazon EC2.
+# * Capacity – If there are not enough unused EC2 instances to meet the demand for spot instances, Amazon EC2 interrupts spot instances. Amazon EC2 determines the order in which the instances are interrupted.
 # * Constraints – If your request includes a constraint such as a launch group or an Availability Zone group, these spot instances are terminated as a group when the constraint can no longer be met.
 #
-# Generally, most spot instances are obtained for around 2 hours (median), with the floor being around 20 minutes, and the ceiling of unbounded duration.
+# Generally, most spot instances are obtained for around 2 hours (median), with the floor being about 20 minutes and the ceiling of unbounded duration.
 #
 # .. note::
 #
@@ -23,7 +23,7 @@ Using Spot/Preemptible Instances
 # - AWS: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/spot-requests.html
 # - GCP: https://cloud.google.com/compute/docs/instances/create-start-preemptible-instance
 #
-# If auto-scaling group (ASG) is set up, you may want to isolate the tasks that you want to trigger on spot/preemptible instances from the regular workloads.
+# If an auto-scaling group (ASG) is set up, you may want to isolate the tasks you want to trigger on spot/preemptible instances from the regular workloads.
 # This can be done by setting taints and tolerations using the `config <https://github.com/flyteorg/flyteplugins/blob/60b94c688ef2b98aa53a9224b529ac672af04540/go/tasks/pluginmachinery/flytek8s/config/config.go#L84-L92>`__ available at ``flyteorg/flyteplugins`` repo.
 #
 # .. admonition:: What's an ASG for a spot/preemptible instance?
@@ -34,11 +34,11 @@ Using Spot/Preemptible Instances
 # What Are Interruptible Tasks?
 # =============================
 #
-# If specified, the ``interruptible flag`` is added to the task definition, and signals to the Flyte engine that it may be scheduled on machines that may be preempted, such as AWS spot instances. This is low-hanging fruit for any cost-savings initiative. 
+# If specified, the ``interruptible flag`` is added to the task definition and signals to the Flyte engine that it may be scheduled on machines that may be preempted, such as AWS spot instances. This is low-hanging fruit for any cost-savings initiative. 
 #
 # Setting Interruptible
 # ^^^^^^^^^^^^^^^^^^^^^
-# In order to run your workload on spot, you can set interruptible to ``True``. For example:
+# To run your workload on a spot/preemptible instance, you can set interruptible to ``True``. For example:
 
 @task(cache_version='1', interruptible=True)
 def add_one_and_print(value_to_print: int) -> int:
@@ -48,18 +48,18 @@ def add_one_and_print(value_to_print: int) -> int:
 # By setting this value, Flyte will schedule your task on an auto-scaling group (ASG) with only spot instances. 
 # 
 # .. note::
-#   In the case your task gets preempted, Flyte will retry your task on a non-spot (regular) instance. This retry will not count towards a retry that a user sets.
+# If your task gets preempted, Flyte will retry your task on a non-spot (regular) instance. This retry will not count towards a retry that a user sets.
 #
 # Which Tasks Should Be Set To Interruptible?
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Most Flyte workloads should be good candidates for spot instances. 
-# If your task does NOT exhibit the following properties, then you can set ``interruptible`` to true.
+# If your task does NOT exhibit the following properties, you can set ``interruptible`` to true.
 #
-# * Time sensitive: It needs to run now and can not have any unexpected delays.
-# * Side Effects: The task is not idempotent and retrying will cause issues.
-# * Long Running Tasks: The task takes > 2 hours. Having an interruption during this time frame could potentially waste a lot of computation.
+# * Time-sensitive: It needs to run now and can not have any unexpected delays.
+# * Side Effects: The task is not idempotent, and retrying will cause issues.
+# * Long-Running Tasks: The task takes > 2 hours. Having an interruption during this time frame could potentially waste a lot of computation.
 #
-# In a nutshell, you should use spot/preemptible instances when you want to reduce the total cost of running jobs at the expense of potential delay in execution due to restarts.
+# In a nutshell, you should use spot/preemptible instances when you want to reduce the total cost of running jobs at the expense of potential delays in execution due to restarts.
 
 # %%
 # How to Recover From Interruptions?
@@ -68,3 +68,4 @@ def add_one_and_print(value_to_print: int) -> int:
 # .. NOTE::
 #
 #     Coming soon 🛠
+#
