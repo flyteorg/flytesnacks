1b5fae0e964ded1936c093aaaa087551795a573e
Katrina Rogan <katroganGH@gmail.com>
2021-06-02 17:48:33 -0700

    athena test
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- cookbook/integrations/aws/athena/README.rst
+++ cookbook/integrations/aws/athena/README.rst
@@ -1,7 +1,23 @@
-######
-Athena
-######
+Executing Athena Queries
+=======================
+Flyte backend can be connected withathena. Once enabled it can allow you to query a athena service (e.g. Qubole) and retrieve typed schema (optionally).
+This section will provide how to use the athena Query Plugin using flytekit python
 
-.. NOTE::
+Installation
+------------
 
-    Coming soon 🛠
+To use the flytekit athena plugin simply run the following:
+
+.. prompt:: bash
+
+    pip install flytekitplugins-athena
+
+No Need of a dockerfile
+------------------------
+This plugin is purely a spec and since SQL is completely portable has no need to build a container. Thus this plugin examples do not have any Dockerfile
+
+Configuring the backend to get athena working
+-------------------------------------------
+.. todo:
+
+This is coming soon.
---
+++ cookbook/integrations/aws/athena/athena.py
@@ -0,0 +1,49 @@
+from flytekit import kwtypes, task, workflow
+from flytekit.types.schema import FlyteSchema
+
+from flytekitplugins.athena import AthenaTask, AthenaConfig
+
+# %%
+# This is the world's simplest query. Note that in order for registration to work properly, you'll need to give your
+# Athena task a name that's unique across your project/domain for your Flyte installation.
+athena_task_no_io = AthenaTask(
+    name="sql.athena.no_io",
+    inputs={},
+    query_template="""
+        select 1
+    """,
+    output_schema_type=None,
+    task_config=AthenaConfig(database="mnist"),
+)
+
+
+@workflow
+def no_io_wf():
+    return athena_task_no_io()
+
+
+athena_task_w_out = AthenaTask(
+    name="sql.athena.w_io",
+    inputs=kwtypes(limit=int),
+    task_config=AthenaConfig(database="vaccinations"),
+    query_template="""
+    SELECT * FROM vaccinations limit {{ .inputs.limit }}
+    """,
+    output_schema_type=FlyteSchema,
+)
+
+
+@task
+def print_athena_schema(s: FlyteSchema):
+    df = s.open().all()
+    print(df.to_markdown())
+
+
+@workflow
+def full_hive_demo_wf(limit: int) -> FlyteSchema:
+    demo_schema = athena_task_w_out(limit=limit)
+    print_athena_schema(s=demo_schema)
+    return demo_schema
+
+
+
