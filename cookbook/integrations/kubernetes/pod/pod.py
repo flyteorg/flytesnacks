9a47c74757d8cb5ac89e8e6c33b8b040474bd0b1
Katrina Rogan <katroganGH@gmail.com>
2021-06-08 17:21:23 -0700

    update
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- cookbook/integrations/kubernetes/pod/pod.py
+++ cookbook/integrations/kubernetes/pod/pod.py
@@ -82,7 +82,7 @@ def generate_pod_spec_for_task():
     task_config=Pod(
         pod_spec=generate_pod_spec_for_task(), primary_container_name="primary"
     ),
-    requests=Resources(cpu="0.1", mem="1000"),
+    requests=Resources(cpu="0.1", mem="1000"), limits=Resources(cpu="0.1", mem="2000"),
 )
 def my_pod_task(attempts: int) -> str:
     # The code defined in this task will get injected into the primary container.
