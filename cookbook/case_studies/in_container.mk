06e286118a2ea15e4199a4ac51d2345ffddb990b
Yuvraj <code@evalsocket.dev>
2021-06-26 01:06:59 +0530

    fix typo
    Signed-off-by: Yuvraj <code@evalsocket.dev>

--- cookbook/case_studies/in_container.mk
+++ cookbook/case_studies/in_container.mk
@@ -10,7 +10,7 @@ $(SERIALIZED_PB_OUTPUT_DIR): clean
 .PHONY: serialize
 serialize: $(SERIALIZED_PB_OUTPUT_DIR)
 	pyflyte --config /root/sandbox.config serialize workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
-	
+
 .PHONY: fast_serialize
 fast_serialize: $(SERIALIZED_PB_OUTPUT_DIR)
 	pyflyte --config /root/sandbox.config serialize fast workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
--- cookbook/in_container.mk
+++ cookbook/in_container.mk
@@ -11,14 +11,6 @@ $(SERIALIZED_PB_OUTPUT_DIR): clean
 serialize: $(SERIALIZED_PB_OUTPUT_DIR)
 	pyflyte --config /root/sandbox.config serialize workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
 
-.PHONY: register
-register: serialize 
-	flytectl register file $(SERIALIZED_PB_OUTPUT_DIR)/* -d development -p ${PROJECT} --continueOnError -v ${VERSION} --k8ServiceAccount=${SERVICE_ACCOUNT} --outputLocationPrefix=${OUTPUT_DATA_PREFIX}
-
 .PHONY: fast_serialize
 fast_serialize: $(SERIALIZED_PB_OUTPUT_DIR)
 	pyflyte --config /root/sandbox.config serialize fast workflows -f $(SERIALIZED_PB_OUTPUT_DIR)
-
-.PHONY: fast_register
-fast_register: fast_serialize
-	flyte-cli fast-register-files -h ${FLYTE_HOST} ${INSECURE_FLAG} -p ${PROJECT} -d development --kubernetes-service-account ${SERVICE_ACCOUNT} --output-location-prefix ${OUTPUT_DATA_PREFIX} --additional-distribution-dir ${ADDL_DISTRIBUTION_DIR} $(SERIALIZED_PB_OUTPUT_DIR)/*
