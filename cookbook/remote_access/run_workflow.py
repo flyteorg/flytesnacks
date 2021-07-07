a2fd0b0b0258fb5043425c9afe9eac0939a96497
pmahindrakar-oss <prafulla.mahindrakar@gmail.com>
2021-07-07 11:37:53 +0530

    [PR Into 302] Added documentation for running task, launchplans ,inspecting and debgging them (#316)
    * Added documentation for running task, launchplans ,inspecting and debugging them
    
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
    
    * Incorporated the feedback
    
    Signed-off-by: pmahindrakar-oss <prafulla.mahindrakar@gmail.com>
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- cookbook/remote_access/run_workflow.py
+++ cookbook/remote_access/run_workflow.py
@@ -1,4 +1,8 @@
 """
 Running a Workflow
 ------------------
+
+Workflows on their own are not runnable directly. A launchplan is always bound to a workflow and you can use launchplans to **launch** a workflow. For cases in which you want the launchplan to have the same arguments as a workflow, if you are using one of the SDK's to author your workflows - like flytekit, flytekit-java etc, then they should automatically create a ``default launchplan`` for the workflow. A ``default launchplan`` has identical name as the workflow and all argument defaults are similar. Thus you can use the default launchplan to trigger the workflow.
+Tasks can be run using the launch command. One difference is that, launchplans cannot be associated with a task, this is to avoid triggers and scheduling.
+
 """
