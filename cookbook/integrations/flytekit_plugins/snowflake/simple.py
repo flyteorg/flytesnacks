6bd2afbed7517fe749af168e05769a1c2f4f05f9
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2021-07-08 09:59:56 -0700

    Conditions testing both paths (#326)
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

---
+++ cookbook/integrations/flytekit_plugins/snowflake/simple.py
@@ -0,0 +1,22 @@
+from flytekit import workflow, Secret
+from flytekit.types.schema import FlyteSchema
+from flytekitplugins.snowflake import SnowflakeConfig, SnowflakeTask
+
+tk = SnowflakeTask(
+    "test_snowflake",
+    query_template='select * from "DEMO_DB"."PUBLIC"."SAMPLE"',
+    task_config=SnowflakeConfig(
+        account_name="hoa61102",
+        username="haytham",
+        password=Secret(group="snowflake", key="password"),
+        db="DEMO_DB",
+        schema="PUBLIC",
+        warehouse="COMPUTE_WH"
+    ),
+    output_schema_type=FlyteSchema,
+)
+
+
+@workflow
+def snowflake_query() -> FlyteSchema:
+    return tk()
