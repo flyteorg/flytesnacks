d9e8e3b63458f55a21b04ecfe79c362011b7263f
Haytham Abuelfutuh <haytham@afutuh.com>
2021-07-12 09:21:47 -0700

    trying
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- cookbook/integrations/flytekit_plugins/snowflake/simple.py
+++ cookbook/integrations/flytekit_plugins/snowflake/simple.py
@@ -1,17 +1,24 @@
 from flytekit import workflow, Secret
 from flytekit.types.schema import FlyteSchema
-from flytekitplugins.snowflake import SnowflakeConfig, SnowflakeTask
+from flytekitplugins.sqlalchemy import SqlAlchemyConfig, SqlAlchemyTask
 
-tk = SnowflakeTask(
+tk = SqlAlchemyTask(
     "test_snowflake",
     query_template='select * from "DEMO_DB"."PUBLIC"."SAMPLE"',
-    task_config=SnowflakeConfig(
-        account_name="hoa61102",
-        username="haytham",
-        password=Secret(group="snowflake", key="password"),
-        db="DEMO_DB",
-        schema="PUBLIC",
-        warehouse="COMPUTE_WH"
+    task_config=SqlAlchemyConfig(
+        uri='snowflake://{user}:{password}@{account}/'.format(
+            user='haytham',
+            password='<your_password>',
+            account='hoa61102',
+        ),
+        secret_connect_args={
+            "password": Secret(group="snowflake", key="password"),
+        }
+        # username="haytham",
+        # password=Secret(group="snowflake", key="password"),
+        # db="DEMO_DB",
+        # schema="PUBLIC",
+        # warehouse="COMPUTE_WH"
     ),
     output_schema_type=FlyteSchema,
 )
