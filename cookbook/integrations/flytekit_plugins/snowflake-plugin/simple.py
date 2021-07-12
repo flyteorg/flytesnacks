ed94999d2a3be89b1849bcb9dd10336339b7e098
Haytham Abuelfutuh <haytham@afutuh.com>
2021-07-12 11:47:52 -0700

    try flytekitplugin
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- cookbook/integrations/flytekit_plugins/snowflake-plugin/simple.py
+++ cookbook/integrations/flytekit_plugins/snowflake-plugin/simple.py
@@ -1,3 +1,4 @@
+from flyteidl.core.security_pb2 import Secret
 from flytekit import workflow
 from flytekit.types.schema import FlyteSchema
 from flytekitplugins.snowflake import SnowflakeConfig, SnowflakeTask
@@ -6,19 +7,12 @@ tk = SnowflakeTask(
     "test_snowflake",
     query_template='select * from "DEMO_DB"."PUBLIC"."SAMPLE"',
     task_config=SnowflakeConfig(
-        uri='snowflake://{user}:{password}@{account}/'.format(
-            user='haytham',
-            password='<your_password>',
-            account='hoa61102',
-        ),
-        # secret_connect_args={
-        #     "password": Secret(group="snowflake", key="password"),
-        # }
-        # username="haytham",
-        # password=Secret(group="snowflake", key="password"),
-        # db="DEMO_DB",
-        # schema="PUBLIC",
-        # warehouse="COMPUTE_WH"
+        account_name='hoa61102',
+        username="haytham",
+        password=Secret(group="snowflake", key="password"),
+        db="DEMO_DB",
+        schema="PUBLIC",
+        warehouse="COMPUTE_WH"
     ),
     output_schema_type=FlyteSchema,
 )
