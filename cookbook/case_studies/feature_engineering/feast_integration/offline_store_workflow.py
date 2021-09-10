408bcfdc34938178b4926a119cc830a1191a97c4
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2021-09-10 14:56:12 -0700

    Refactoring + retrieve offline features
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- cookbook/case_studies/feature_engineering/feast_integration/offline_store_workflow.py
+++ cookbook/case_studies/feature_engineering/feast_integration/offline_store_workflow.py
@@ -1,10 +1,36 @@
-from feast import FeatureStore, Entity, FeatureView, FileSource, ValueType, Feature, RepoConfig, repo_config
+import pandas as pd
+from datetime import datetime
+from feast import FeatureStore, Entity, FeatureView, FileSource, ValueType, Feature, RepoConfig
+from feast.infra.online_stores.sqlite import SqliteOnlineStoreConfig
 from flytekit import task, workflow
 from flytekit.extras.sqlite3.task import SQLite3Config, SQLite3Task
+from flytekit.types.file.file import FlyteFile
 from flytekit.types.schema import FlyteSchema
 from datetime import timedelta
 
+# TODO: find a better way to define these features.
+FEAST_FEATURES = [
+    "horse_colic_stats:rectal temperature",
+    "horse_colic_stats:total protein",
+    "horse_colic_stats:peripheral pulse",
+    "horse_colic_stats:surgical lesion",
+    "horse_colic_stats:abdominal distension",
+    "horse_colic_stats:nasogastric tube",
+    "horse_colic_stats:outcome",
+    "horse_colic_stats:packed cell volume",
+    "horse_colic_stats:nasogastric reflux PH",
+]
 DATABASE_URI = "https://cdn.discordapp.com/attachments/545481172399030272/861575373783040030/horse_colic.db.zip"
+
+def _build_feature_store() -> FeatureStore:
+    config = RepoConfig(
+        # Notice the use of an s3 uri to store the registry
+        registry="s3://feast-integration/registry.db",
+        project=f"horsecolic",
+        provider="local",
+        # online_store=SqliteOnlineStoreConfig(),
+    )
+    return FeatureStore(config=config)
 
 
 sql_task = SQLite3Task(
@@ -18,16 +44,10 @@ sql_task = SQLite3Task(
 )
 
 @task
-def store_offline(dataframe: FlyteSchema) -> (str, str):
-    # Build RepoConfig
-    config = RepoConfig(
-        registry="s3://feast-integration/registry.db",
-        project=f"horse-colic",
-        provider="local",
-        # online_store=DatastoreOnlineStoreConfig(namespace="integration_test"),
-    )
-    fs = FeatureStore(config=config)
+def all_together(dataframe: FlyteSchema):
     horse_colic_entity = Entity(name="Hospital Number", value_type=ValueType.STRING)
+
+    print(f"dataframe.remote_path={dataframe.remote_path}")
 
     horse_colic_feature_view = FeatureView(
         name="horse_colic_stats",
@@ -43,21 +63,144 @@ def store_offline(dataframe: FlyteSchema) -> (str, str):
             Feature(name="packed cell volume", dtype=ValueType.FLOAT),
             Feature(name="nasogastric reflux PH", dtype=ValueType.FLOAT),
         ],
-        input=FileSource(
+        batch_source=FileSource(
             path=str(dataframe.remote_path),
             event_timestamp_column="timestamp",
         ),
         ttl=timedelta(days=1),
     )
 
+    fs = _build_feature_store()
+
+    # Ingest the data into feast
     fs.apply([horse_colic_entity, horse_colic_feature_view])
-    return repo_path, horse_colic_feature_view.name
+
+    entity_df = pd.DataFrame.from_dict(
+        {
+            "Hospital Number": [
+                "530101",
+                "5290409",
+                "5291329",
+                "530051",
+                "529518",
+                "530101",
+                "529340",
+                "5290409",
+                "530034",
+            ],
+            "event_timestamp": [
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 7, 5, 11, 36, 1),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 7, 5, 11, 50, 40),
+                datetime(2021, 6, 25, 16, 36, 27),
+            ],
+        }
+    )
+
+    fs = _build_feature_store()
+    retrieval_job = fs.get_historical_features(
+        entity_df=entity_df,
+        features=FEAST_FEATURES,
+    )
+    df = retrieval_job.to_df()
+
+
+@task
+def store_offline(dataframe: FlyteSchema):
+    horse_colic_entity = Entity(name="Hospital Number", value_type=ValueType.STRING)
+
+    print(f"dataframe.remote_path={dataframe.remote_path}")
+
+    horse_colic_feature_view = FeatureView(
+        name="horse_colic_stats",
+        entities=["Hospital Number"],
+        features=[
+            Feature(name="rectal temperature", dtype=ValueType.FLOAT),
+            Feature(name="total protein", dtype=ValueType.FLOAT),
+            Feature(name="peripheral pulse", dtype=ValueType.FLOAT),
+            Feature(name="surgical lesion", dtype=ValueType.STRING),
+            Feature(name="abdominal distension", dtype=ValueType.FLOAT),
+            Feature(name="nasogastric tube", dtype=ValueType.STRING),
+            Feature(name="outcome", dtype=ValueType.STRING),
+            Feature(name="packed cell volume", dtype=ValueType.FLOAT),
+            Feature(name="nasogastric reflux PH", dtype=ValueType.FLOAT),
+        ],
+        batch_source=FileSource(
+            path=str(dataframe.remote_path),
+            event_timestamp_column="timestamp",
+        ),
+        ttl=timedelta(days=1),
+    )
+
+    fs = _build_feature_store()
+
+    # Ingest the data into feast
+    fs.apply([horse_colic_entity, horse_colic_feature_view])
+
+@task
+def load_historical_features() -> FlyteSchema:
+    entity_df = pd.DataFrame.from_dict(
+        {
+            "Hospital Number": [
+                "530101",
+                "5290409",
+                "5291329",
+                "530051",
+                "529518",
+                "530101",
+                "529340",
+                "5290409",
+                "530034",
+            ],
+            "event_timestamp": [
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 7, 5, 11, 36, 1),
+                datetime(2021, 6, 25, 16, 36, 27),
+                datetime(2021, 7, 5, 11, 50, 40),
+                datetime(2021, 6, 25, 16, 36, 27),
+            ],
+        }
+    )
+
+    fs = _build_feature_store()
+    retrieval_job = fs.get_historical_features(
+        entity_df=entity_df,
+        features=FEAST_FEATURES,
+    )
+    return retrieval_job.to_df()
+
+
+@task
+def convert_timestamp_column(dataframe: FlyteSchema, timestamp_column: str) -> FlyteSchema:
+    df = dataframe.open().all()
+    df[timestamp_column] = pd.to_datetime(df[timestamp_column])
+    return df
+
 
 @workflow
 def load_data_into_offline_store():
     # Load parquet file from sqlite task
     df = sql_task()
-    a, b = store_offline(dataframe=df)
+
+    # Need to convert timestamp column in the underlying dataframe, otherwise its type is written as
+    # string. There is probably a better way of doing this conversion.
+    converted_df = convert_timestamp_column(dataframe=df, timestamp_column="timestamp")
+
+    # TODO: remove. Used only for debugging.
+    # all_together(dataframe=converted_df)
+
+    store_offline(dataframe=converted_df)
+
+    feature_data = load_historical_features()
 
 if __name__ == '__main__':
     print(f"{load_data_into_offline_store()}")
