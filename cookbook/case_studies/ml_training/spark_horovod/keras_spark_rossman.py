26574e79577bf898c8f0f452c8a3b7e362f46e99
Katrina Rogan <katroganGH@gmail.com>
2021-09-21 10:47:54 -0700

    debug
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- cookbook/case_studies/ml_training/spark_horovod/keras_spark_rossman.py
+++ cookbook/case_studies/ml_training/spark_horovod/keras_spark_rossman.py
@@ -561,9 +561,9 @@ def train_model(categorical_cols, continuous_cols, hp, train_df, len_vocab, max_
     # train_df._downloader(train_df.remote_path, train_df.local_path)
     opened_train_df = train_df.open(pyspark.sql.DataFrame).all()
 
+    opened_train_df.cache()
     num_rows = opened_train_df.count()
     print(f"num rows {num_rows}")
-    opened_train_df.cache()
     print(f" local_path {train_df.local_path}")
     print(f" contents of local path {os.listdir(train_df.local_path)}")
     # store = Store.create(train_df.local_path)
