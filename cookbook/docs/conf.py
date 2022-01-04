4c510c788b3ef99588d6d9c19297737a4b3b2169
Kevin Su <pingsutw@apache.org>
2022-01-05 00:23:21 +0800

    Structured dataset example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cookbook/docs/conf.py
+++ cookbook/docs/conf.py
@@ -59,6 +59,7 @@ class CustomSorter(FileNameSortKey):
         # Type System
         "flyte_python_types.py",
         "schema.py",
+        "structured_dataset.py"
         "typed_schema.py",
         "custom_objects.py",
         "enums.py",
