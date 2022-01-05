fab7ce9796eeb8e3a4b39b5e02ad903b35c020e7
SmritiSatyanV <smriti@union.ai>
2022-01-05 19:10:53 +0530

    Rolled back to changes

--- cookbook/core/scheduled_workflows/lp_schedules.py
+++ cookbook/core/scheduled_workflows/lp_schedules.py
@@ -1,9 +1,8 @@
 """
 Scheduling Workflows
 --------------------
-For background on launch plans, refer to :ref:`launchplans <flyte:divedeep-launchplans>`.
-Launch plans can be set to run automatically on a schedule using the flyte native scheduler.
-For workflows that depend on knowing the kick-off time, Flyte also supports passing in the scheduled time (not the actual time, which may be a few seconds off) as an argument to the workflow. 
+:ref:`flyte:divedeep-launchplans` can be set to run automatically on a schedule using the Flyte Native Scheduler.
+For workflows that depend on knowing the kick-off time, Flyte also supports passing in the scheduled time (not the actual time, which may be a few seconds off) as an argument to the workflow.
 
 Check out a demo of how Native Scheduler works:
 
