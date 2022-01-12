ccc36ba0dcbb039588fc289dc04540a90db44551
SmritiSatyanV <smriti@union.ai>
2022-01-12 13:40:45 +0530

    Multi-region house prediction
    Changes based on comments

--- cookbook/case_studies/ml_training/house_price_prediction/multiregion_house_price_predictor.py
+++ cookbook/case_studies/ml_training/house_price_prediction/multiregion_house_price_predictor.py
@@ -1,36 +1,53 @@
 """
-Predicting House Price in Multiple Regions with XGBoost and Dynamic Workflows
-------------------------------------------------------------------------------
+.. _single_region_house_prediction:
 
-In this example, you'll use the house price prediction model for one region to expand it to multiple regions. 
+Predicting House Price in a Region Using XGBoost
+------------------------------------------------
+
+`XGBoost <https://xgboost.readthedocs.io/en/latest/>`__ is an optimized distributed gradient boosting library designed to be efficient, flexible, and portable. 
+It uses `gradient boosting <https://en.wikipedia.org/wiki/Gradient_boosting>`__ technique to implement Machine Learning algorithms.
+
+In this tutorial, we will understand how to predict house prices using XGBoost, and Flyte.
+
+We will split the generated dataset into train, test and validation set. 
+
+Next, we will create three Flyte tasks, that will:
+
+1. Generate house details, and split the dataset.
+
+2. Train the model using XGBoost.
+
+3. Generate predictions.
+
+Let's get started with the example!
 """
 
 # %%
-# Importing the Libraries
-# ========================
-# First, import all the required libraries.
-import typing
-
-import pandas as pd
-from flytekit import Resources, dynamic, workflow
-
-try:
-    from .house_price_predictor import (
-        generate_and_split_data,
-        fit,
-        predict,
-    )
-except ImportError:
-    from house_price_predictor import (
-        generate_and_split_data,
-        fit,
-        predict,
-    )
+# Install the following libraries before running the model (locally):
+#
+# .. code-block:: python
+#
+#       pip install scikit-learn
+#       pip install joblib
+#       pip install xgboost
 
 # %%
-# Initializing the Variables
-# ===========================
-# Initialize the variables to be used while building the model.
+# First, let's import the required packages into the environment.
+import typing
+
+import os
+import flytekit
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from flytekit import Resources, task, workflow
+from flytekit.types.file import JoblibSerializedFile
+from typing import Tuple
+
+# %%
+# We initialize variables that represent columns in the dataset. We use these variables to build the model, since they affect the house prices.
 NUM_HOUSES_PER_LOCATION = 1000
 COLUMNS = [
     "PRICE",
@@ -41,114 +58,206 @@ COLUMNS = [
     "LOT_ACRES",
     "GARAGE_SPACES",
 ]
-LOCATIONS = [
-    "NewYork_NY",
-    "LosAngeles_CA",
-    "Chicago_IL",
-    "Houston_TX",
-    "Dallas_TX",
-    "Phoenix_AZ",
-    "Philadelphia_PA",
-    "SanAntonio_TX",
-    "SanDiego_CA",
-    "SanFrancisco_CA",
-]
+MAX_YEAR = 2021
+# divide the data into train, validation, and test datasets in specific ratio.
+SPLIT_RATIOS = [0.6, 0.3, 0.1]
 
 # %%
-# Task: Generating & Splitting the Data for Multiple Regions
-# ============================================================
-# Call the previously defined helper functions to generate and split the data. Finally, return the DataFrame objects.
+# Data Generation
+# =====================
+#
+# We define a function to compute the price of a house based on multiple factors (such as ``number of bedrooms``, ``number of bathrooms``, ``area``, ``garage space`` and ``year built``).
+def gen_price(house) -> int:
+    _base_price = int(house["SQUARE_FEET"] * 150)
+    _price = int(
+        _base_price
+        + (10000 * house["NUM_BEDROOMS"])
+        + (15000 * house["NUM_BATHROOMS"])
+        + (15000 * house["LOT_ACRES"])
+        + (15000 * house["GARAGE_SPACES"])
+        - (5000 * (MAX_YEAR - house["YEAR_BUILT"]))
+    )
+    return _price
 
+
+# %%
+# Next, using the above function, let's generate a DataFrame object that constitutes all the houses' details.
+def gen_houses(num_houses) -> pd.DataFrame:
+    _house_list = []
+    for _ in range(num_houses):
+        _house = {
+            "SQUARE_FEET": int(np.random.normal(3000, 750)),
+            "NUM_BEDROOMS": np.random.randint(2, 7),
+            "NUM_BATHROOMS": np.random.randint(2, 7) / 2,
+            "LOT_ACRES": round(np.random.normal(1.0, 0.25), 2),
+            "GARAGE_SPACES": np.random.randint(0, 4),
+            "YEAR_BUILT": min(MAX_YEAR, int(np.random.normal(1995, 10))),
+        }
+        _price = gen_price(_house)
+        # column names/features 
+        _house_list.append(
+            [
+                _price,
+                _house["YEAR_BUILT"],
+                _house["SQUARE_FEET"],
+                _house["NUM_BEDROOMS"],
+                _house["NUM_BATHROOMS"],
+                _house["LOT_ACRES"],
+                _house["GARAGE_SPACES"],
+            ]
+        )
+    # convert the list to a DataFrame    
+    _df = pd.DataFrame(
+        _house_list,
+        columns=COLUMNS,
+    )
+    return _df
+
+# %%
+# Data Preprocessing and Splitting
+# ===================================
+#
+# We create two columns from the dataset- `feature` and `target`. 
+#
+#Next, we split these columns into train and test subsets. We also split the input DataFrame into train, validation, and test datasets.
+def split_data(
+    df: pd.DataFrame, seed: int, split: typing.List[float]
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+
+    seed = seed
+    val_size = split[1]  # 0.3 
+    test_size = split[2] # 0.1
+
+    num_samples = df.shape[0]
+    # retain the features, skip the target column
+    x1 = df.values[:num_samples, 1:]  
+    # retain the target column
+    y1 = df.values[:num_samples, :1]  
+
+    # divide the features and target column into random train and test subsets, based on `test_size` 
+    x_train, x_test, y_train, y_test = train_test_split(
+        x1, y1, test_size=test_size, random_state=seed
+    )
+    # divide the train data into train and validation subsets, based on `test_size`
+    x_train, x_val, y_train, y_val = train_test_split(
+        x_train,
+        y_train,
+        test_size=(val_size / (1 - test_size)), # here, `test_size` computes to 0.3
+        random_state=seed,
+    )
+
+    # reassemble the datasets by placing `target` as first column and `features` in the subsequent columns
+    _train = np.concatenate([y_train, x_train], axis=1)
+    _val = np.concatenate([y_val, x_val], axis=1)
+    _test = np.concatenate([y_test, x_test], axis=1)
+
+    # return three DataFrames with train, test, and validation data
+    return (
+        pd.DataFrame(
+            _train,
+            columns=COLUMNS,
+        ),
+        pd.DataFrame(
+            _val,
+            columns=COLUMNS,
+        ),
+        pd.DataFrame(
+            _test,
+            columns=COLUMNS,
+        ),
+    )
+
+# %%
+# Let's create a ``NamedTuple`` that maps variable names to their respective data type.
 dataset = typing.NamedTuple(
     "GenerateSplitDataOutputs",
-    train_data=typing.List[pd.DataFrame],
-    val_data=typing.List[pd.DataFrame],
-    test_data=typing.List[pd.DataFrame],
+    train_data=pd.DataFrame,
+    val_data=pd.DataFrame,
+    test_data=pd.DataFrame,
 )
 
-
-@dynamic(cache=True, cache_version="0.1", limits=Resources(mem="600Mi"))
-def generate_and_split_data_multiloc(
-    locations: typing.List[str],
-    number_of_houses_per_location: int,
-    seed: int,
-) -> dataset:
-    train_sets = []
-    val_sets = []
-    test_sets = []
-    for _ in locations:
-        _train, _val, _test = generate_and_split_data(
-            number_of_houses=number_of_houses_per_location, seed=seed
-        )
-        train_sets.append(
-            _train,
-        )
-        val_sets.append(
-            _val,
-        )
-        test_sets.append(
-            _test,
-        )
-    return train_sets, val_sets, test_sets
+# %%
+# We define a task to generate and split data into train, test, and validation DataFrames.
+@task(cache=True, cache_version="0.1", limits=Resources(mem="600Mi"))
+def generate_and_split_data(number_of_houses: int, seed: int) -> dataset:
+    _houses = gen_houses(number_of_houses)
+    return split_data(_houses, seed, split=SPLIT_RATIOS)
 
 
 # %%
-# Dynamic Workflow: Training the XGBoost Model & Generating the Predictions for Multiple Regions
-# ===============================================================================================
+# Training
+# ==========
 #
-# Fit the model to the data and generate predictions (two functionalities in a single task to make it more powerful!)
-#
-# Note: You can also use two separate methods to fit the model and generate predictions but this basically means parallelizing an entire set of tasks.
-@dynamic(cache=True, cache_version="0.1", limits=Resources(mem="600Mi"))
-def parallel_fit_predict(
-    multi_train: typing.List[pd.DataFrame],
-    multi_val: typing.List[pd.DataFrame],
-    multi_test: typing.List[pd.DataFrame],
-) -> typing.List[typing.List[float]]:
-    preds = []
+# Now, we serialize the XGBoost model using `joblib` and store the model in a `dat` file.
+@task(cache_version="1.0", cache=True, limits=Resources(mem="600Mi"))
+def fit(loc: str, train: pd.DataFrame, val: pd.DataFrame) -> JoblibSerializedFile:
 
-    for loc, train, val, test in zip(LOCATIONS, multi_train, multi_val, multi_test):
-        model = fit(loc=loc, train=train, val=val)
-        preds.append(predict(test=test, model_ser=model))
+    # fetch the features and target columns from the train dataset
+    x = train[train.columns[1:]]
+    y = train[train.columns[0]]
 
-    return preds
+    # fetch the features and target columns from the validation dataset
+    eval_x = val[val.columns[1:]]
+    eval_y = val[val.columns[0]]
+
+    m = XGBRegressor()
+    # fit the model to the train data
+    m.fit(x, y, eval_set=[(eval_x, eval_y)])
+
+    working_dir = flytekit.current_context().working_directory
+    fname = os.path.join(working_dir, f"model-{loc}.joblib.dat")
+    joblib.dump(m, fname)
+    return JoblibSerializedFile(path=fname)
 
 
 # %%
-# Defining the Workflow
-# ======================
-# Include the following three steps in the workflow:
+# Generating Predictions
+# ========================
 #
-# #. Generate and split the data (Step 3)
-# #. Parallelly fit the XGBoost model and generate predictions for multiple regions (Step 4)
+# Next, we unserialize the XGBoost model using `joblib` to generate the predictions.
+@task(cache_version="1.0", cache=True, limits=Resources(mem="600Mi"))
+def predict(
+    test: pd.DataFrame,
+    model_ser: JoblibSerializedFile,
+) -> typing.List[float]:
+
+    # load the model
+    model = joblib.load(model_ser)
+
+    # load the test data
+    x_df = test[test.columns[1:]]
+
+    # generate predictions
+    y_pred = model.predict(x_df).tolist()
+
+    # return the predictions
+    return y_pred
+
+
+# %%
+# Lastly, we define a workflow to run the pipeline.
 @workflow
-def multi_region_house_price_prediction_model_trainer(
+def house_price_predictor_trainer(
     seed: int = 7, number_of_houses: int = NUM_HOUSES_PER_LOCATION
-) -> typing.List[typing.List[float]]:
+) -> typing.List[float]:
 
-    # Generate and split the data
-    split_data_vals = generate_and_split_data_multiloc(
-        locations=LOCATIONS,
-        number_of_houses_per_location=number_of_houses,
-        seed=seed,
+    # generate the data and split it into train test, and validation data
+    split_data_vals = generate_and_split_data(
+        number_of_houses=number_of_houses, seed=seed
     )
 
-    # Parallelly fit the XGBoost model for multiple regions
-    # Generate predictions for multiple regions
-    predictions = parallel_fit_predict(
-        multi_train=split_data_vals.train_data,
-        multi_val=split_data_vals.val_data,
-        multi_test=split_data_vals.test_data,
+    # fit the XGBoost model
+    model = fit(
+        loc="NewYork_NY", train=split_data_vals.train_data, val=split_data_vals.val_data
     )
+
+    # generate predictions
+    predictions = predict(model_ser=model, test=split_data_vals.test_data)
 
     return predictions
 
 
 # %%
-# Trigger the workflow locally by calling the workflow function.
+# We can run the workflow locally provided the required libraries are installed. The output would be a list of house prices, generated using the XGBoost model.
 if __name__ == "__main__":
-    print(multi_region_house_price_prediction_model_trainer())
-
-
-# %%
-# The output will be a list of lists (one list per region) of house price predictions.
+    print(house_price_predictor_trainer())
