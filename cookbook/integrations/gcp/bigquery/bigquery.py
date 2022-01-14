27a1680b6f5aa3bb582eb4992622567d29457fbc
Kevin Su <pingsutw@apache.org>
2022-01-14 22:54:40 +0800

    Updated example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cookbook/integrations/gcp/bigquery/bigquery.py
+++ cookbook/integrations/gcp/bigquery/bigquery.py
@@ -4,8 +4,9 @@ BigQuery Query
 
 This example shows how to use a Flyte BigQueryTask to execute a query.
 """
+import pandas as pd
 
-from flytekit import kwtypes, workflow
+from flytekit import kwtypes, workflow, StructuredDataset, task
 from flytekitplugins.bigquery import BigQueryConfig, BigQueryTask
 # %%
 # This is the world's simplest query. Note that in order for registration to work properly, you'll need to give your
@@ -14,7 +15,6 @@ bigquery_task_no_io = BigQueryTask(
     name="sql.bigquery.no_io",
     inputs={},
     query_template="SELECT 1",
-    output_schema_type=None,
     task_config=BigQueryConfig(ProjectID="flyte", Location="us-west1-b"),
 )
 
@@ -32,18 +32,30 @@ def no_io_wf():
 # Let's look out how we can parameterize our query to filter results for a specific transaction version, provided as a user input
 # specifying a version.
 
+DogeCoinDataset = StructuredDataset[kwtypes(hash=str, size=int, block_number=int)]
+
 bigquery_task_templatized_query = BigQueryTask(
     name="sql.bigquery.w_io",
     # Define inputs as well as their types that can be used to customize the query.
     inputs=kwtypes(version=int),
+    output_structured_dataset_type=DogeCoinDataset,
     task_config=BigQueryConfig(ProjectID="flyte", Location="us-west1-b"),
     query_template="SELECT * FROM `bigquery-public-data.crypto_dogecoin.transactions` WHERE @version = 1 LIMIT 10;",
 )
 
 
+# %%
+# StructuredDataset transformer can convert query result to pandas dataframe here.
+# We can also change "pandas.dataframe" to "pyarrow.Table" to convert result to Arrow table.
+@task
+def convert_bq_table_to_pandas_dataframe(sd: DogeCoinDataset) -> pd.DataFrame:
+    return sd.open(pd.DataFrame).all()
+
+
 @workflow
-def full_bigquery_wf(version: int):
-    return bigquery_task_templatized_query(version=version)
+def full_bigquery_wf(version: int) -> pd.DataFrame:
+    sd = bigquery_task_templatized_query(version=version)
+    return convert_bq_table_to_pandas_dataframe(sd=sd)
 
 # %%
 # Check query result on bigquery console: ``https://console.cloud.google.com/bigquery``
