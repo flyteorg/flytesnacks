f49964dfb537e5c7aea437874a54bfc9764e982d
Kevin Su <pingsutw@apache.org>
2022-04-20 00:03:11 +0800

    wip
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cookbook/core/flyte_basics/deck.py
+++ cookbook/core/flyte_basics/deck.py
@@ -25,6 +25,7 @@ from flytekit.deck.renderer import TopFrameRenderer
 iris_df = px.data.iris()
 
 
+# %
 # Here we first create new deck called demo, and use box renderer to show box plot on demo deck.
 # Then use MarkdownRenderer to render md_text, and append HTML to default deck.
 @task()
@@ -39,16 +40,18 @@ def t1() -> str:
 Expected output:
 
 .. prompt:: text
+
   {"asctime": "2022-04-19 23:12:17,266", "name": "flytekit", "levelname": "INFO", "message": "t1 output flytekit deck html to file:///tmp/flyte/20220419_231216/sandbox/local_flytekit/161e15f8c9331e83237bcf52e604697b/deck.html"}
   {"asctime": "2022-04-19 23:12:17,283", "name": "flytekit", "levelname": "INFO", "message": "t2 output flytekit deck html to file:///tmp/flyte/20220419_231216/sandbox/local_flytekit/6d8d1bafe04769592d7b0e212c50bd0e/deck.html"}
+
 """
 
+# %
 # .. figure:: https://keep.google.com/u/0/media/v2/18EI0XAdBEHTFiHAIKX5vG_crQp7wOVIR1zPFDeFVhV-An87zi4-VNaOn-O3VzQ/1J-nKZvxl0U9ZPRBis9gJGt19y4wfasOucImobO74zzICnwP3s8kztijpd7abON4?sz=512&accept=image%2Fgif%2Cimage%2Fjpeg%2Cimage%2Fjpg%2Cimage%2Fpng%2Cimage%2Fwebp
 #   :alt: Demo Deck
 #   :class: with-shadow
 #
 #   Demo Deck
-
 # .. figure:: https://keep.google.com/u/0/media/v2/1OWpuyWk_udxUp1Qu12EAJotuJLGjq9ygyTTZcpP3FyRpalpSOKiEsLOUHe01ZA/1o3ccc_6NQ6v3cqzf0Z2l5Z54nL4BrP6mM0TvVCX9CMPXaIy-BmwI6C3JwnzB1w?sz=512&accept=image%2Fgif%2Cimage%2Fjpeg%2Cimage%2Fjpg%2Cimage%2Fpng%2Cimage%2Fwebp
 #   :alt: Default Deck
 #   :class: with-shadow
@@ -56,11 +59,13 @@ Expected output:
 #   Default Deck
 
 
+# %
 # Use Annotated to override default renderer, and display top 10 rows of dataframe
 @task()
 def t2() -> Annotated[pd.DataFrame, TopFrameRenderer(10)]:
     return iris_df
 
+# %
 # .. figure:: https://keep.google.com/u/0/media/v2/17Gzd6gM-EaionxXzwDZvXeMt3d4l69YPN-ubR3Mp2RSKoIDUOZvF3DTTmZPA6vQ/1DXoZXSh4NlRZ7XBCuc7tGlYk2tLdOiuWI-YjuH7MCEs3FSiDc4OCg4kX36Dixw?sz=512&accept=image%2Fgif%2Cimage%2Fjpeg%2Cimage%2Fjpg%2Cimage%2Fpng%2Cimage%2Fwebp
 #   :alt: Output Deck
 #   :class: with-shadow
