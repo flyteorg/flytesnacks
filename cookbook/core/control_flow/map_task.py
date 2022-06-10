4126d296d79e7b0cd8d75bcbfead65f6d1fb98d4
Marc Paquette <marc@union.ai>
2022-06-10 16:05:29 -0400

    Update map_task.py
    Use dataclass instead of NamedTuple.
    
    Signed-off-by: Marc Paquette <marc@union.ai>

--- cookbook/core/control_flow/map_task.py
+++ cookbook/core/control_flow/map_task.py
@@ -16,7 +16,7 @@ Let's look at an example now!
 
 # %%
 # First, import the libraries.
-import typing
+from typing import List
 
 from flytekit import Resources, map_task, task, workflow
 
@@ -36,19 +36,19 @@ def a_mappable_task(a: int) -> str:
 # %%
 # Also define a task to reduce the mapped output to a string.
 @task
-def coalesce(b: typing.List[str]) -> str:
+def coalesce(b: List[str]) -> str:
     coalesced = "".join(b)
     return coalesced
 
 
 # %%
 # We send ``a_mappable_task`` to be repeated across a collection of inputs to the :py:func:`~flytekit:flytekit.map_task` function.
-# In the example, ``a`` of type ``typing.List[int]`` is the input.
+# In the example, ``a`` of type ``List[int]`` is the input.
 # The task ``a_mappable_task`` is run for each element in the list.
 #
 # ``with_overrides`` is useful to set resources for individual map task.
 @workflow
-def my_map_workflow(a: typing.List[int]) -> str:
+def my_map_workflow(a: List[int]) -> str:
     mapped_out = map_task(a_mappable_task)(a=a).with_overrides(
         requests=Resources(mem="300Mi"),
         limits=Resources(mem="500Mi"),
@@ -65,8 +65,31 @@ if __name__ == "__main__":
     print(f"{result}")
 
 # %%
-# By default, the map task uses the K8s Array plugin. Map tasks can also run on alternate execution backends, such as `AWS Batch <https://docs.flyte.org/en/latest/deployment/plugin_setup/aws/batch.html#deployment-plugin-setup-aws-array>`__,
+# When defining a map task, avoid calling other tasks in it. Flyte
+# can't accurately register tasks that call other tasks.  While Flyte
+# will correctly execute a task that calls other tasks, it will not be
+# able to give the full performance advantages. This is
+# especially true for map tasks.
+#
+# In this example, the map task ``suboptimal_mappable_task`` would not
+# give you the best performance.
+@task
+def upperhalf(a: int) -> int:
+    return a / 2 + 1
+
+@task
+def suboptimal__mappable_task(a: int) -> str:
+    inc = upperhalf(a=a)
+    stringified = str(inc)
+    return stringified
+
+# %%
+#
+# By default, the map task uses the K8s Array plugin. Map tasks can
+# also run on alternate execution backends, such as
+# `AWS Batch <https://docs.flyte.org/en/latest/deployment/plugin_setup/aws/batch.html#deployment-plugin-setup-aws-array>`__,
 # a provisioned service that can scale to great sizes.
+
 
 # %%
 # Map a task with multiple inputs
@@ -75,21 +98,26 @@ if __name__ == "__main__":
 # You might need to map a task with multiple inputs but you only need
 # to change a few of the inputs for the mapping.
 #
-# For example, we have a task that takes 3 inputs:
-#
+# For example, we have a task that takes 3 inputs.
 @task
-def full_mappable_task(quantity: int, price: float, shipping: float) -> float:
+def multi_input_task(quantity: int, price: float, shipping: float) -> float:
     return quantity * price * shipping
-
 
 # %%
 # But we only want to map this task with the ``quantity`` input
-# while the other inputs stay the same. Since a map task may accept only
+# while the other inputs stay the same. Since a map task accepts only
 # one input, we can do this by creating a new task that prepares the
-# map task's inputs:
-from typing import List, NamedTuple
+# map task's inputs.
+#
+# We start by putting the inputs in a Dataclass and
+# ``dataclass_json``. We also define our helper task to prepare the map
+# task's inputs.
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
-class MapInput(NamedTuple):
+@dataclass
+@dataclass_json
+class MapInput:
     quantity: int
     price: float
     shipping: float
@@ -99,19 +127,16 @@ def prepare_map_inputs(list_q: List[int], p: float, s: float) -> List[MapInput]:
     return [MapInput(q, p, s) for q in list_q]
 
 # %%
-# When defining a map task, avoid calling other tasks in it. A map
-# task that calls other tasks prevents Flyte from accurately
-# registration. While Flyte can execute the task correctly, it will
-# not be able to give the full performance advantages of a map task.
-#
-# In this example, the map task `suboptimal_mappable_task` would not
-# give you the best performance:
+# Then we refactor ``multi_input_task``. Instead of 3 inputs, this
+# map-friendly task has a single input.
 @task
-def discount(quantity: float) -> float:
-    return quantity * 0.10
+def mappable_task(input: MapInput) -> float:
+    return input.quantity * input.price * input.shipping
 
-@task
-def suboptimal_mappable_task(quantity: int, price: float, shipping: float) -> float:
-    return quantity * price * shipping - discount(quantity)
-
+# %%
+# Our workflow prepares a new list of inputs for the map task.
+@workflow
+def multiple_workflow(list_q: List[int], p: str, s: float) -> List[int]:
+    map_input = prepare_map_inputs(list_q=list_q, p=p, s=s)
+    return map_task(mappable_task)(input=map_input)
 
