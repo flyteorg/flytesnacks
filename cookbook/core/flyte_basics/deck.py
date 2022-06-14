aa67859d5ec30c4c389341d5f2cdf6bfabe17c43
SmritiSatyanV <smriti@union.ai>
2022-06-14 22:44:12 +0530

    Updated deck.py
    Ran Pre-commit
    Signed-off-by: SmritiSatyanV <smriti@union.ai>

--- cookbook/core/flyte_basics/deck.py
+++ cookbook/core/flyte_basics/deck.py
@@ -2,32 +2,36 @@
 Flyte Decks
 -------------
 
-Deck enable users to get customizable and default visibility into their tasks.
+Deck enables users to get customizable and default visibility into their tasks.
 
-Flytekit contains various renderers (FrameRenderer, MarkdownRenderer) that can
-generate html files. For example, FrameRenderer can render a DataFrame as an HTML table,
-MarkdownRenderer can convert Markdown string to HTML
+Flytekit contains various renderers (FrameRenderer, MarkdownRenderer) that
+generate HTML files. For example, FrameRenderer renders a DataFrame as an HTML table,
+and MarkdownRenderer converts Markdown string to HTML.
 
-Each task has a least three decks (input, output, default). Input/output decks are
+Each task has at least three decks (input, output, and default). Input/output decks are
 used to render tasks' input/output data, and the default deck is used to render line plots,
 scatter plots or markdown text. In addition, users can create new decks to render
 their data with custom renderers.
+Let's dive into an example.
 """
+import flytekit
+
+# %%
+# Import the dependencies.
 import pandas as pd
 import plotly.express as px
+from flytekit import task, workflow
+from flytekit.deck.renderer import TopFrameRenderer
 from flytekitplugins.deck.renderer import BoxRenderer, MarkdownRenderer
 from typing_extensions import Annotated
 
-import flytekit
-from flytekit import task, workflow
-from flytekit.deck.renderer import TopFrameRenderer
-
+# Fetch iris data.
 iris_df = px.data.iris()
 
 
 # %%
-# Here we first create new deck called demo, and use box renderer to show box plot on demo deck.
-# Then use MarkdownRenderer to render md_text, and append HTML to default deck.
+# Create a new deck called ``demo``, and use the box renderer to display the box plot on demo deck.
+# Use MarkdownRenderer to render ``md_text``, and append HTML to the default deck.
 @task()
 def t1() -> str:
     md_text = "#Hello Flyte\n##Hello Flyte\n###Hello Flyte"
@@ -39,10 +43,10 @@ def t1() -> str:
 # %%
 # .. note::
 #
-#   To see the log output, the env var `FLYTE_SDK_LOGGING_LEVEL` should be set to 20
+#   To see the log output, the ``FLYTE_SDK_LOGGING_LEVEL`` environment variable should be set to 20.
 
 # %%
-# Expected output:
+# Expected output
 #
 # .. prompt:: text
 #
@@ -50,7 +54,7 @@ def t1() -> str:
 #   {"asctime": "2022-04-19 23:12:17,283", "name": "flytekit", "levelname": "INFO", "message": "t2 task creates flyte deck html to file:///tmp/flyte/20220419_231216/sandbox/local_flytekit/6d8d1bafe04769592d7b0e212c50bd0e/deck.html"}
 
 # %%
-# Open the `deck.html` file
+# Open the ``deck.html`` file.
 
 # %%
 # .. figure:: https://i.ibb.co/4Sxts8w/Screen-Shot-2022-04-19-at-11-24-55-PM.png
@@ -64,13 +68,14 @@ def t1() -> str:
 
 
 # %%
-# Use Annotated to override default renderer, and display top 10 rows of dataframe
+# Use ``Annotated`` to override the default renderer, and display top 10 rows of dataframe.
 @task()
 def t2() -> Annotated[pd.DataFrame, TopFrameRenderer(10)]:
     return iris_df
 
+
 # %%
-# Open Flyte Deck on FlyteConsole
+# Open the Flyte Deck on FlyteConsole.
 
 # %%
 # .. figure:: https://i.ibb.co/7yCJnSs/flyteconsole.png
@@ -84,12 +89,15 @@ def t2() -> Annotated[pd.DataFrame, TopFrameRenderer(10)]:
 #   :class: with-shadow
 #
 
-
+# %%
+# Define the workflow.
 @workflow
 def wf():
     t1()
     t2()
 
 
+# %%
+# Run the code locally.
 if __name__ == "__main__":
     wf()
