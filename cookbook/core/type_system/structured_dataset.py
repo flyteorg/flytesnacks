485ffc3be7852c04cf6cff543d8d5ffb86fb9512
SmritiSatyanV <smriti@union.ai>
2022-06-15 12:05:20 +0530

    Updates based on comments
    Signed-off-by: SmritiSatyanV <smriti@union.ai>

--- cookbook/core/type_system/structured_dataset.py
+++ cookbook/core/type_system/structured_dataset.py
@@ -50,8 +50,8 @@ subset_cols = kwtypes(Age=int)
 
 
 # %%
-# We define two tasks— one returns a pandas DataFrame, and the other, a :py:class:`flytekit.types.schema.FlyteSchema`.
-# The DataFrames will be serialized to an intermediate format like parquet before sending them to the other tasks.
+# We define two tasks, one returns a pandas DataFrame and the other a :py:class:`flytekit.types.schema.FlyteSchema`.
+# The DataFrames are serialized to an intermediate format (like parquet file) before sending them to the other tasks.
 @task
 def get_df(a: int) -> Annotated[pd.DataFrame, superset_cols]:
     """
@@ -76,7 +76,7 @@ def get_schema_df(a: int) -> FlyteSchema[superset_cols]:
 
 # %%
 # Next, we define a task that opens a structured dataset by calling ``all()``.
-# When we invoke ``all()``, Flyte engine downloads parquet file on s3, and deserializes it to ``pandas.dataframe``.
+# When we invoke ``all()``, the Flyte engine downloads parquet file on s3, and deserializes it to ``pandas.dataframe``.
 #
 # .. note::
 #   * Despite the input type of the task being ``StructuredDataset``, it can also accept FlyteSchema as input.
