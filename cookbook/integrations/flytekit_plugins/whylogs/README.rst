4ae7174888ed86617b17f04507e1a3bfc5f6627e
murilommen <murilommen@gmail.com>
2022-08-01 17:59:11 -0300

    Creating flytesnacks integration structure
    details: added todos, created empty files and directories
    Signed-off-by: murilommen <murilommen@gmail.com>

---
+++ cookbook/integrations/flytekit_plugins/whylogs/README.rst
@@ -0,0 +1,4 @@
+# TODO What is the integration about? Its features, etc.
+# TODO Why do we need this integration? How is it going to benefit the Flyte users?
+# TODO Showcase the uniqueness of the integration
+# TODO How to install the plugin?
---
+++ cookbook/integrations/flytekit_plugins/whylogs/__init__.py
@@ -0,0 +1,7 @@
+# Actual examples
+# TODO create type_example.py
+# TODO create renderer_example.py
+
+# Documentation
+# TODO create README
+# TODO review Contributing demands: https://github.com/murilommen/flytesnacks/blob/master/cookbook/docs/contribute.rst#id1
