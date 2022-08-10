85deda0704cefe3b05d1fa327864ddb5cfddd691
Samhita Alla <aallasamhita@gmail.com>
2022-08-10 14:52:39 +0530

    working version
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- cookbook/core/control_flow/chain_entities.py
+++ cookbook/core/control_flow/chain_entities.py
@@ -2,32 +2,67 @@
 Chain Flyte Entities
 --------------------
 
-Data passing between tasks need not necessarily happen through parameters.
-In such a case, if you want to explicitly construct the tasks' dependency, flytekit provides a mechanism to chain tasks using the ``>>`` operator.
+Data passing between tasks or workflows need not necessarily happen through parameters.
+In such a case, if you want to explicitly construct the dependency, flytekit provides a mechanism to chain Flyte entities using the ``>>`` operator.
 
-In this example, let's enforce an order for ``read()`` to happen after ``write()``.
+Tasks
+^^^^^
+
+Let's enforce an order for ``read()`` to happen after ``write()``, and for ``write()`` to happen after ``create_bucket()``.
+
+.. note::
+    To run the example locally, spin up the demo cluster using ``flytectl demo start``.
 """
 
 # %%
-# First, import the necessary dependencies.
+# Import the necessary dependencies.
+import logging
+from io import StringIO
+
+import boto3
 import pandas as pd
-
 from flytekit import task, workflow
-from flytekit.core.node_creation import create_node
 
-DATABASE = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+logger = logging.getLogger(__file__)
+
+CSV_FILE = "iris.csv"
+BUCKET_NAME = "chain-flyte-entities"
+
+
+BOTO3_CLIENT = boto3.client(
+    "s3",
+    aws_access_key_id="minio",
+    aws_secret_access_key="miniostorage",
+    use_ssl=False,
+    endpoint_url="http://minio.flyte:9000",
+)
+
 # %%
-# Define a ``read()`` task to read from the file.
+# Create an s3 bucket.
+# This task exists just for the sandbox case.
+@task(cache=True, cache_version="1.0")
+def create_bucket():
+    try:
+        BOTO3_CLIENT.create_bucket(Bucket=BUCKET_NAME)
+    except BOTO3_CLIENT.exceptions.BucketAlreadyOwnedByYou:
+        logger.info(f"Bucket {BUCKET_NAME} has already been created by you.")
+        pass
+
+
+# %%
+# Define a ``read()`` task to read from the s3 bucket.
 @task
 def read() -> pd.DataFrame:
-    data = pd.read_csv(DATABASE)
+    data = pd.read_csv(
+        BOTO3_CLIENT.get_object(Bucket=BUCKET_NAME, Key=CSV_FILE)["Body"]
+    )
     return data
 
+
 # %%
-# Define a ``write()`` task to write to the file. Let's assume we are populating the CSV file.
-@task
+# Define a ``write()`` task to write the dataframe to a CSV file in the s3 bucket.
+@task(cache=True, cache_version="1.0")
 def write():
-    # dummy code
     df = pd.DataFrame(  # noqa : F841
         data={
             "sepal_length": [5.3],
@@ -37,51 +72,63 @@ def write():
             "species": ["setosa"],
         }
     )
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer)
+    BOTO3_CLIENT.put_object(
+        Body=csv_buffer.getvalue(), Bucket=BUCKET_NAME, Key=CSV_FILE
+    )
 
-    # Write the data to a database
-    # pd.to_csv("...")
 
 # %%
-# We want to enforce an order here: ``write()`` followed by ``read()``. 
-# Since no data-passing happens between the tasks, we use ``>>`` operator on the nodes.
+# We want to enforce an order here: ``create_bucket()`` followed by ``write()`` followed by ``read()``.
+# Since no data-passing happens between the tasks, use ``>>`` operator on the tasks.
 @workflow
 def chain_tasks_wf() -> pd.DataFrame:
-    write_node = create_node(write)
-    read_node = create_node(read)
+    create_bucket_promise = create_bucket()
+    write_promise = write()
+    read_promise = read()
 
-    write_node >> read_node
+    create_bucket_promise >> write_promise
+    write_promise >> read_promise
 
-    return read_node
+    return read_promise
 
-# %% 
-# How to Chain SubWorkflows?
-# ^^^^^^^^^^^^^^^^^^^^^^^^^
-# 
-# Similar to tasks, you can also chain `SubWorkflows <https://docs.flyte.org/projects/cookbook/en/latest/auto/core/control_flow/subworkflows.html>`__.
 
 # %%
+# SubWorkflows
+# ^^^^^^^^^^^^
+#
+# Similar to tasks, you can chain :ref:`subworkflows <subworkflows>`.
 @workflow
 def write_sub_workflow():
     write()
+
 
 @workflow
 def read_sub_workflow() -> pd.DataFrame:
     return read()
 
+
 # %%
-# Use ``>>`` operator on the nodes to chain subworkflows.
+# Use ``>>`` to chain the subworkflows.
 @workflow
 def chain_workflows_wf() -> pd.DataFrame:
+    create_bucket_promise = create_bucket()
     write_sub_wf = write_sub_workflow()
     read_sub_wf = read_sub_workflow()
 
+    create_bucket_promise >> write_sub_wf
     write_sub_wf >> read_sub_wf
 
     return read_sub_wf
 
+
 #%%
-# Finally, run the workflow locally.
+# Run the workflows locally.
 if __name__ == "__main__":
     print(f"Running {__file__} main...")
     print(f"Running chain_tasks_wf()... {chain_tasks_wf()}")
     print(f"Running chain_workflows_wf()... {chain_workflows_wf()}")
+
+# %%
+# .. run-example-cmds::
