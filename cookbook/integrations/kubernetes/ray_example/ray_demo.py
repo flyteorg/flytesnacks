38cd33c5cc9abd693371aba091a12e5f3298b349
Kevin Su <pingsutw@apache.org>
2022-08-23 03:05:01 +0800

    add example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ cookbook/integrations/kubernetes/ray_example/ray_demo.py
@@ -0,0 +1,36 @@
+import typing
+
+import ray
+from flytekit import Resources, task, workflow
+from flytekitplugins.ray import HeadNodeConfig, RayJobConfig, WorkerNodeConfig
+
+
+@ray.remote
+def f(x):
+    return x * x
+
+
+ray_config = RayJobConfig(
+    head_node_config=HeadNodeConfig(ray_start_params={"log-color": "True"}),
+    worker_node_config=[WorkerNodeConfig(group_name="ray-group", replicas=2)],
+    runtime_env={"pip": ["numpy", "pandas"]},  # or runtime_env="./requirements.txt"
+)
+
+@task(task_config=ray_config, limits=Resources(mem="2000Mi", cpu="1"))
+def ray_task(n: int) -> typing.List[int]:
+    futures = [f.remote(i) for i in range(n)]
+    return ray.get(futures)
+
+@task
+def task(n: typing.List[int]) -> typing.List[int]:
+    return [i*i for i in range(n)]
+
+@workflow
+def ray_workflow(n: int) -> typing.List[int]:
+    return ray_task(n=n)
+
+
+# %%
+# We can run the code locally wherein Flyte creates a standalone Ray cluster locally.
+if __name__ == "__main__":
+    print(ray_workflow(n=10))
