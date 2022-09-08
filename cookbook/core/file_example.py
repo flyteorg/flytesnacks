0a72165b32c090c05cb153ad87a1558daad7b031
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-09-08 10:58:17 -0700

    add third file
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- cookbook/core/file_example.py
+++ cookbook/core/file_example.py
@@ -8,7 +8,7 @@ from flytekit.types.file import FlyteFile
 
 
 @task
-def files_task() -> typing.Tuple[FlyteFile, FlyteFile]:
+def files_task() -> typing.Tuple[FlyteFile, FlyteFile, FlyteFile]:
     fd, filename1 = tempfile.mkstemp()
     with os.fdopen(fd, "w") as tmp:
         tmp.write("hello world 1")
@@ -17,11 +17,16 @@ def files_task() -> typing.Tuple[FlyteFile, FlyteFile]:
     with os.fdopen(fd, "w") as tmp:
         tmp.write("hello world 2")
 
-    print(f"File 1: {filename1} File 2: {filename2}")
+    fd, filename3 = tempfile.mkstemp()
+    with os.fdopen(fd, "w") as tmp:
+        tmp.write("hello world 3")
 
-    return filename1, FlyteFile(filename2)
+    print(f"File 1: {filename1} File 2: {filename2} File 3: {filename3}")
+
+    return filename1, FlyteFile(filename2), FlyteFile(filename3,
+                                                      remote_path="s3://union-opencompute-open-compute2-playground/yt/specific_file_location_1")
 
 
 @workflow
-def files_wf() -> typing.Tuple[FlyteFile, FlyteFile]:
+def files_wf() -> typing.Tuple[FlyteFile, FlyteFile, FlyteFile]:
     return files_task()
