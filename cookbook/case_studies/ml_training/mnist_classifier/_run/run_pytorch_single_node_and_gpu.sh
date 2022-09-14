d47c364cf3b083684a7d903280b99726a8b1a156
SmritiSatyanV <smriti@union.ai>
2022-09-14 13:18:20 +0530

    Deleted .sh and .py files
    Deleted .sh and .py files for tutorials that don't work today
    Signed-off-by: SmritiSatyanV <smriti@union.ai>

--- cookbook/case_studies/ml_training/mnist_classifier/_run/run_pytorch_single_node_and_gpu.sh
+++
@@ -1,2 +0,0 @@
-# shellcheck shell=ksh
-pyflyte run --remote --image ghcr.io/flyteorg/flytecookbook:mnist_classifier-latest pytorch_single_node_and_gpu.py pytorch_training_wf
--- cookbook/case_studies/ml_training/spark_horovod/_run/run_keras_spark_rossmann_estimator.sh
+++
@@ -1,2 +0,0 @@
-# shellcheck shell=ksh
-pyflyte run --remote --image ghcr.io/flyteorg/flytecookbook:spark_horovod-latest keras_spark_rossmann_estimator.py horovod_spark_wf
