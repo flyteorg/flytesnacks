d129fce6c3e0a8c388f167b3358d7485e5b94cde
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2022-10-27 15:23:47 +0530

    Update deck.py
    Signed-off-by: Utsav Paul <91927689+Smartmind12@users.noreply.github.com>

--- cookbook/core/flyte_basics/deck.py
+++ cookbook/core/flyte_basics/deck.py
@@ -18,7 +18,7 @@ Let's dive into an example.
 # %%
 # .. note::
 #
-#   Flyte Decks is an opt-in feature; to enable it, set `disable_deck` to `False` in the task params.
+#   Flyte Decks is an opt-in feature; to enable it, set ``disable_deck`` to ``False`` in the task params.
 
 # %%
 # Import the dependencies.
