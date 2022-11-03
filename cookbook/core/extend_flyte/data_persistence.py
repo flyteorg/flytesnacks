f2b45a21c6720db91fb5578954fab04b7f414236
Kevin Su <pingsutw@apache.org>
2022-11-02 17:29:20 -0700

    wip
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ cookbook/core/extend_flyte/data_persistence.py
@@ -0,0 +1,11 @@
+"""
+.. _extend-flyte-persistence-plugin:
+
+##########################
+Writing Persistence Plugin
+##########################
+
+Currently flytekit uses `awscli <https://aws.amazon.com/cli/>`__ or `gcsutil <https://cloud.google.com/storage/docs/gsutil>`__
+to upload/download
+
+"""
