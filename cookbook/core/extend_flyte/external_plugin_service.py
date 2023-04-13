bd06f26b106ac4dd13c4982026579b42fa5135b5
Kevin Su <pingsutw@apache.org>
2023-04-13 07:23:47 -0700

    update docs
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cookbook/core/extend_flyte/external_plugin_service.py
+++ cookbook/core/extend_flyte/external_plugin_service.py
@@ -12,23 +12,21 @@ It is hard to implement backend plugins, especially for data-scientists & MLE’
 of Golang. Also, performance requirements, maintenance and development is cumbersome.
 
 We build a new component in flyte called ``external-plugin-system``, some goal behind this,
-- Plugins should be easy to author - no need of code generation, using tools that MLEs and Data Scientists are not accustomed to using.
-- Most important plugins for Flyte today are plugins that communicate with external services.
-- It should be possible to test these plugins independently and also deploy them privately.
-- It should be possible for users to use backend plugins for local development, especially in flytekit and unionML
-- It should be possible to author plugins in any language.
-- Plugins should be scalable
-- Plugins should have a very simple API
-- Plugins should show up in the UI (extra details)
+* Plugins should be easy to author - no need of code generation, using tools that MLEs and Data Scientists are not accustomed to using.
+* Most important plugins for Flyte today are plugins that communicate with external services.
+* It should be possible to test these plugins independently and also deploy them privately.
+* It should be possible for users to use backend plugins for local development, especially in flytekit and unionML
+* It should be possible to author plugins in any language.
+* Plugins should be scalable
+* Plugins should have a very simple API
+* Plugins should show up in the UI (extra details)
 
 Overview
 ========
-The External Plugin System is a Python-based plugin registry that uses a gRPC server. Users and Propeller can send gRPC
- requests to this registry to run jobs, such as BigQuery and Databricks. Furthermore, the registry is stateless,
-  which makes it easy to scale the system up or down.
+The External Plugin System is a Python-based plugin registry that uses a gRPC server. Users and Propeller can send gRPC requests to this registry to run jobs, such as BigQuery and Databricks. Furthermore, the registry is stateless, which makes it easy to scale the system up or down.
 
-.. figure:: https://file.notion.so/f/s/f1cdc46e-88f9-4b11-bec5-19629074d5a9/Untitled.png?id=0b3e45f9-7d32-47a0-8a74-a4ecf1ca00e8&table=block&spaceId=644c92b6-6f64-49fa-96f0-e0a1ece8cb94&expirationTimestamp=1680617867407&signature=1y-rlyarnDWJ0twBolvUFMMA8vTqIkD-HkEMfNtHhpM&downloadName=Untitled.png
-  :alt: MLflow UI
+.. figure:: https://keep.google.com/u/0/media/v2/1mpmXqb-qqPI6HyPypu-AL-2pOgtbCnQwrhIePJN8l8qyChU1jERKHzrvrnE5eA/1pC3cjUR48XV-IvWbVC6V3QrkCfHCiLijC6m41TjoIKxy2D4XGFJZFlVWAAwDOX0?sz=512&accept=image%2Fgif%2Cimage%2Fjpeg%2Cimage%2Fjpg%2Cimage%2Fpng%2Cimage%2Fwebp
+  :alt: External-Plugin-System
   :class: with-shadow
 
 Register a new plugin
@@ -36,8 +34,7 @@ Register a new plugin
 
 Flytekit Interface Specification
 --------------------------------
-To register a new backend plugin, user have to extend `BackendPluginBase` in the flytekit backend module.
- Users have to implement below 3 methods, and all the call should be idempotent.
+To register a new backend plugin, user have to extend `BackendPluginBase` in the flytekit backend module. Users have to implement below 3 methods, and all the call should be idempotent.
 
 - `create` is used to launch a new task. People can use gRPC, REST, or SDK to create a task.
 - `get` is used to get the jobID (like BigQuery Job ID or Databricks task ID).
@@ -105,14 +102,16 @@ Update Helm Chart
           - external-plugin-service
         default-for-task-types:
           - bigquery_query_job_task: external-plugin-service
-          - custom_plugin: external-plugin-service
+          - custom_task: external-plugin-service
 
     flyteplugins-service:
       # By default, all the request will be sent to the default endpoint.
-      defaultGrpcEndpoint: flyte-sandbox.flyte.svc.cluster.local:80
+      defaultGrpcEndpoint: "dns:///external-plugin-service-production.flyte.svc.cluster.local:80"
+      supportedTaskTypes:
+        - custom_task
       endpointForTaskTypes:
         # It will override the default grpc endpoint for bigquery endpoint, which means propeller will send create request to this endpoint.
-        - bigquery_query_job_task: "flyte-2.flyte.svc.cluster.local:80"
+        - bigquery_query_job_task: "dns:///external-plugin-service-development.flyte.svc.cluster.local:80"
 
 3. Restart the FlytePropeller
 """
