55dda5d2ed1e96dd3caedea07c71b404c80666b6
Kevin Su <pingsutw@apache.org>
2023-04-26 14:45:57 -0700

    Image spec example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- cookbook/core/containerization/image_sepc_example.py
+++ cookbook/core/containerization/image_sepc_example.py
@@ -8,24 +8,24 @@ Building Image without Dockerfile
 
 Image Spec is a way to specify how to build a container image without a Dockerfile. The image spec by default will be
 converted to an `Envd <https://envd.tensorchord.ai/>`__ config, and the `Envd builder
-<https://github.com/flyteorg/flytekit/blob/master/plugins/flytekit-envd/flytekitplugins/envd/image_builder.py#L12-L34
->`__ will build the image for you. However, you can also register your own builder to build the image using other tools.
+<https://github.com/flyteorg/flytekit/blob/master/plugins/flytekit-envd/flytekitplugins/envd
+/image_builder.py#L12-L34>`__ will build the image for you. However, you can also register your own builder to build
+the image using other tools.
 
 For every :py:class:`flytekit.PythonFunctionTask` task or a task decorated with the ``@task`` decorator,
 you can specify rules for binding container images. By default, flytekit binds a single container image, i.e.,
 the `default Docker image <https://ghcr.io/flyteorg/flytekit>`__, to all tasks. To modify this behavior,
 use the ``container_image`` parameter available in the :py:func:`flytekit.task` decorator, and pass an
-:py:class:`flytekit.image_spec.ImageSpec` object.
+`ImageSpec`.
 
- Flytekit will build the image before registering the workflow, and replace the image name in the workflow spec with
- the newly built image name.
+ Flytekit checks the container registry first to see if the image already exists. By doing so, it avoids having to
+ rebuild the image over and over again. If the image does not exist, flytekit will build the image before registering
+ the workflow, and replace the image name in the workflow spec with the newly built image name.
 
-.. note::
-    If the Docker image is not available publicly, refer to :ref:`Pulling Private Images<private_images>`.
 """
 
 from flytekit import task, workflow
-from flytekit.image_spec.image_spec import ImageSpec
+from flytekit import ImageSpec
 import pandas as pd
 
 
@@ -34,12 +34,10 @@ import pandas as pd
 # ==========
 #
 # People can specify python packages, apt packages, and environment variables. Those packages will be added on top of
-# the `default image <https://github.com/flyteorg/flytekit/blob/master/Dockerfile>`__.
-image_spec = ImageSpec(packages=["pandas", "numpy"],
-                       python_version="3.9",
-                       apt_packages=["git"],
-                       env={"Debug": "True"},
-                       registry="pingsutw")
+# the `default image <https://github.com/flyteorg/flytekit/blob/master/Dockerfile>`__. You can also override the
+# default image by passing ``base_image`` parameter to the ``ImageSpec``.
+image_spec = ImageSpec(packages=["pandas", "numpy"], python_version="3.9", apt_packages=["git"],
+                       env={"Debug": "True"}, registry="pingsutw")
 
 
 # %%
@@ -60,20 +58,22 @@ def t2():
 # You can also pass imageSpec yaml file to the ``pyflyte run`` or ``pyflyte register`` command to override it.
 # For instance:
 #
-# .. yaml::
-#   python_version: 3.11
-#   packages:
-#     - pandas
-#     - numpy
-#   apt_packages:
-#     - git
-#   env:
-#     Debug: "True"
+# .. code-block:: yaml
+#
+#    python_version: 3.11
+#    packages:
+#      - pandas
+#      - numpy
+#    apt_packages:
+#      - git
+#    env:
+#      Debug: "True"
 #
 #
 # .. code-block::
 #
-#   pyflyte run --remote --image image.yaml image_spec_example.py wf
+#    pyflyte run --remote --image image.yaml image_spec_example.py wf
+#
 @task()
 def t3():
     print("flyte")
