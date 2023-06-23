f7a5eb44a39468d3fac270cb518ca98875968315
Niels Bantilan <niels.bantilan@gmail.com>
2023-06-23 15:00:37 -0400

    wip integrations
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- cookbook/tmp.sh
+++ cookbook/tmp.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-files=$(find ./integrations -mindepth 1 -maxdepth 5 -type f -name Dockerfile -exec dirname '{}' \;)
-mkdir -p ./_integrations
+# files=$(find ./integrations -mindepth 1 -maxdepth 5 -type f -name Dockerfile -exec dirname '{}' \;)
+# mkdir -p ./_integrations
+# for file in $files
+# do
+#     example_name=$(basename -- $file)
+#     mv $file ./_integrations/$example_name
+# done
+
+files=$(find ./_integrations -mindepth 1 -maxdepth 5 -type f -name Dockerfile -exec dirname '{}' \;)
 for file in $files
 do
     example_name=$(basename -- $file)
-    mv $file ./_integrations/$example_name
+    (cd $file && mkdir -p $example_name && mv *.py $example_name)
 done
