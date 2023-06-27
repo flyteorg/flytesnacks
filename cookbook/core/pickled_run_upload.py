6af66b8b492461b096376424dacda962ec6b1cd4
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-06-27 15:15:55 -0700

    more pickle tests
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- cookbook/core/pickled_run_upload.py
+++ cookbook/core/pickled_run_upload.py
@@ -1,6 +1,7 @@
 import typing
 
 from flytekit import task, workflow
+from flytekit.types.directory import FlyteDirectory
 
 
 @task
@@ -8,10 +9,16 @@ def t1(a: typing.Any) -> int:
     return a
 
 
+@task
+def print_folder(f: FlyteDirectory) -> None:
+    print(f"folder object is {f}")
+
+
 @workflow
-def wf(a: typing.Any = 1) -> int:
+def wf(a: typing.Any = 1, f: FlyteDirectory = "/Users/ytong/temp/data/ddir") -> int:
+    print_folder(f=f)
     return t1(a=a)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wf()
