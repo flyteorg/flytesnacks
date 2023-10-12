7c906a523c3b2e1dc8d35b60ab9e8aa1235e0f3f
jill <gohd8731@gmail.com>
2023-10-12 19:01:47 +0800

    add additional documentation for the with_overrides feature
    Signed-off-by: jill <gohd8731@gmail.com>

--- examples/productionizing/productionizing/customizing_resources.py
+++ examples/productionizing/productionizing/customizing_resources.py
@@ -13,12 +13,14 @@
 # Large datasets may not be able to run locally, so we would want to provide hints to the Flyte backend to request for more memory.
 # This is done by decorating the task with the hints as shown in the following code sample.
 #
-# Tasks can have `requests` and `limits` which mirror the native [equivalents in Kubernetes](https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#requests-and-limits).
+# Tasks can have `task_config` which provides configuration for a specific task types, or `requests` and `limits` which mirror the native [equivalents in Kubernetes](https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/#requests-and-limits).
 # A task can possibly be allocated more resources than it requests, but never more than its limit.
 # Requests are treated as hints to schedule tasks on nodes with available resources, whereas limits
 # are hard constraints.
 #
 # For either a request or limit, refer to the {py:class}`flytekit:flytekit.Resources` documentation.
+#
+# And for task_config,  refer to the {py:func}`flytekit:flytekit.task` documentation.
 #
 # The following attributes can be specified for a `Resource`.
 #
@@ -93,43 +95,54 @@ if __name__ == "__main__":
 # %%
 import typing  # noqa: E402
 
-from flytekit import Resources, task, workflow  # noqa: E402
+from flytekit import Resources, task, workflow, dynamic  # noqa: E402
+from flytekitplugins.kftensorflow import PS, Chief, TfJob, Worker   # noqa: E402
 
 
 # %% [markdown]
 # Define a task and configure the resources to be allocated to it.
-# You can use tasks decorated with memory and storage hints like regular tasks in a workflow.
+# You can use tasks decorated with memory and storage hints like regular tasks in a workflow, or configuration for an {py:class}`flytekitplugins:flytekitplugins.kftensorflow.TfJob` that can run distributed TensorFlow training on Kubernetes.
 # %%
-@task(requests=Resources(cpu="1", mem="200Mi"), limits=Resources(cpu="2", mem="350Mi"))
-def count_unique_numbers_1(x: typing.List[int]) -> int:
-    s = set()
-    for i in x:
-        s.add(i)
-    return len(s)
+@task(
+    task_config=TfJob(
+        num_workers=1,
+        num_ps_replicas=1,
+        num_chief_replicas=1,
+    ),
+    requests=Resources(cpu="1", mem="200Mi"),
+    limits=Resources(cpu="2", mem="350Mi"),
+)
+def run_tfjob() -> str:
 
-
-# %% [markdown]
-# Define a task that computes the square of a number.
-# %%
-@task
-def square_1(x: int) -> int:
-    return x * x
-
+    return "hello world"
 
 # %% [markdown]
 # The `with_overrides` method overrides the old resource allocations.
 # %%
 @workflow
-def my_pipeline(x: typing.List[int]) -> int:
-    return square_1(x=count_unique_numbers_1(x=x)).with_overrides(limits=Resources(cpu="6", mem="500Mi"))
+def my_run() -> str:
+    return run_tfjob().with_overrides(limits=Resources(cpu="6", mem="500Mi"))
 
+
+# %% [markdown]
+# Or you can use `@dynamic` to generate tasks at runtime with any custom configurations you may want.
+# %%
+@dynamic
+def dynamic_run(num_workers: int) -> str:
+    return run_tfjob().with_overrides(task_config=TfJob(
+        num_workers=num_workers,
+        num_ps_replicas=1,
+        num_chief_replicas=1))
+@workflow
+def start_dynamic_run(new_num_workers: int) -> str:
+    return dynamic_run(num_workers=new_num_workers)
 
 # %% [markdown]
 # You can execute the workflow locally.
 # %%
 if __name__ == "__main__":
-    print(count_unique_numbers_1(x=[1, 1, 2]))
-    print(my_pipeline(x=[1, 1, 2]))
+    print(f"Running my_run(): {my_run()}")
+    print(f"Running dynamic_run(num_workers=4): {start_dynamic_run(new_num_workers=4)}")
 
 # %% [markdown]
 # You can see the memory allocation below. The memory limit is `500Mi` rather than `350Mi`, and the
