b1e1fcd3a248a8f0d3f1ae79af24d9dec722c1b6
Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
2023-11-13 14:10:33 -0800

    Add boilerplate and fix flytectl (#1239)
    * add boilerplate
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Fix version of flytectl
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Fix workflow name used to bump boilerplate
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * lint
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * fix indentation
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Use correct reference launchplan
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Pin flytekit version in e2e tests
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Use correct reference task
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- boilerplate/flyte/code_of_conduct/update.sh
+++ boilerplate/flyte/code_of_conduct/update.sh
@@ -7,6 +7,6 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-cp "${DIR}"/CODE_OF_CONDUCT.md "${DIR}"/../../../CODE_OF_CONDUCT.md
+cp "$DIR"/CODE_OF_CONDUCT.md "$DIR"/../../../CODE_OF_CONDUCT.md
