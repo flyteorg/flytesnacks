a7bb0aafe9399c5aa1a9a7d978013500d85dc004
jason.lai <jasonlai1218@gmail.com>
2023-12-12 00:39:47 +0800

    feat: refactor code rendering functionality in `decks.py`
    - Modify the `table_renderer` function in `decks.py`
    - Add a new function `source_code_renderer` in `decks.py`
    - Add a markdown figure for the source code renderer in `decks.py`
    - Add a markdown figure for the table renderer in `decks.py`
    
    Signed-off-by: jason.lai <jasonlai1218@gmail.com>

--- examples/development_lifecycle/development_lifecycle/decks.py
+++ examples/development_lifecycle/development_lifecycle/decks.py
@@ -236,6 +236,29 @@ def table_renderer() -> None:
 
 
 # %% [markdown]
+# :::{figure} https://raw.githubusercontent.com/flyteorg/static-resources/main/flytesnacks/user_guide/flyte_decks_source_code_renderer.png
+# :alt: Source code renderer
+# :class: with-shadow
+# :::
+# #### Source Code Renderer
+# Converts a Source Code into HTML
+# %%
+from flytekitplugins.deck.renderer import SourceCodeRenderer
+import inspect
+
+
+@task(disable_deck=False)
+def source_code_renderer() -> None:
+    file_path = inspect.getsourcefile(frame_renderer.__wrapped__)
+    with open(file_path, 'r') as f:
+        source_code = f.read()
+    flytekit.Deck(
+        "Source Code Renderer",
+        SourceCodeRenderer().to_html(source_code),
+    )
+
+
+# %% [markdown]
 # :::{figure} https://raw.githubusercontent.com/flyteorg/static-resources/main/flytesnacks/user_guide/flyte_decks_table_renderer.png
 # :alt: Table renderer
 # :class: with-shadow
