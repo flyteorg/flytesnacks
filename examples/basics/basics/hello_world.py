eca9f2641e32e63014e2a6d1fa49855ad5acf3f0
nikki everett <nikki@union.ai>
2024-02-22 17:34:47 -0600

    remove markdown from basics files
    Signed-off-by: nikki everett <nikki@union.ai>

--- examples/basics/basics/hello_world.py
+++ examples/basics/basics/hello_world.py
@@ -1,49 +1,16 @@
-# %% [markdown]
-#
-# # Hello, World!
-#
-# ```{eval-rst}
-# .. tags:: Basic
-# ```
-#
-# Let's write a Flyte {py:func}`~flytekit.workflow` that invokes a
-# {py:func}`~flytekit.task` to generate the output "Hello, World!".
-#
-# Flyte tasks are the core building blocks of larger, more complex workflows.
-# Workflows compose multiple tasks – or other workflows –
-# into meaningful steps of computation to produce some useful set of outputs or outcomes.
-#
-# To begin, import `task` and `workflow` from the `flytekit` library.
-# %%
 from flytekit import task, workflow
 
 
-# %% [markdown]
-# Define a task that produces the string "Hello, World!".
-# Simply using the `@task` decorator to annotate the Python function.
-# %%
 @task
 def say_hello() -> str:
     return "Hello, World!"
 
 
-# %% [markdown]
-# You can handle the output of a task in the same way you would with a regular Python function.
-# Store the output in a variable and use it as a return value for a Flyte workflow.
-# %%
 @workflow
 def hello_world_wf() -> str:
     res = say_hello()
     return res
 
 
-# %% [markdown]
-# Run the workflow by simply calling it like a Python function.
-# %%
 if __name__ == "__main__":
     print(f"Running hello_world_wf() {hello_world_wf()}")
-
-
-# %% [markdown]
-# Next, let's delve into the specifics of {ref}`tasks <task>`,
-# {ref}`workflows <workflow>` and {ref}`launch plans <launch_plan>`.
--- examples/basics/basics/named_outputs.py
+++ examples/basics/basics/named_outputs.py
@@ -1,31 +1,6 @@
-# %% [markdown]
-# (named_outputs)=
-#
-# # Named Outputs
-#
-# ```{eval-rst}
-# .. tags:: Basic
-# ```
-#
-# By default, Flyte employs a standardized convention to assign names to the outputs of tasks or workflows.
-# Each output is sequentially labeled as `o1`, `o2`, `o3`, ... `on`, where `o` serves as the standard prefix,
-# and `1`, `2`, ... `n` indicates the positional index within the returned values.
-#
-# However, Flyte allows the customization of output names for tasks or workflows.
-# This customization becomes beneficial when you're returning multiple outputs
-# and you wish to assign a distinct name to each of them.
-#
-# The following example illustrates the process of assigning names to outputs for both a task and a workflow.
-#
-# To begin, import the required dependencies.
-# %%
 from typing import NamedTuple
-
 from flytekit import task, workflow
 
-# %% [markdown]
-# Define a `NamedTuple` and assign it as an output to a task.
-# %%
 slope_value = NamedTuple("slope_value", [("slope", float)])
 
 
@@ -37,9 +12,6 @@ def slope(x: list[int], y: list[int]) -> slope_value:
     return (n * sum_xy - sum(x) * sum(y)) / (n * sum_x_squared - sum(x) ** 2)
 
 
-# %% [markdown]
-# Likewise, assign a `NamedTuple` to the output of `intercept` task.
-# %%
 intercept_value = NamedTuple("intercept_value", [("intercept", float)])
 
 
@@ -51,25 +23,6 @@ def intercept(x: list[int], y: list[int], slope: float) -> intercept_value:
     return intercept
 
 
-# %% [markdown]
-# :::{note}
-# While it's possible to create `NamedTuple`s directly within the code,
-# it's often better to declare them explicitly. This helps prevent potential linting errors in tools like mypy.
-#
-# ```
-# def slope() -> NamedTuple("slope_value", slope=float):
-#     pass
-# ```
-# :::
-#
-# You can easily unpack the `NamedTuple` outputs directly within a workflow.
-# Additionally, you can also have the workflow return a `NamedTuple` as an output.
-#
-# :::{note}
-# Remember that we are extracting individual task execution outputs by dereferencing them.
-# This is necessary because `NamedTuple`s function as tuples and require this dereferencing.
-# :::
-# %%
 slope_and_intercept_values = NamedTuple("slope_and_intercept_values", [("slope", float), ("intercept", float)])
 
 
@@ -80,8 +33,5 @@ def simple_wf_with_named_outputs(x: list[int] = [-3, 0, 3], y: list[int] = [7, 4
     return slope_and_intercept_values(slope=slope_value.slope, intercept=intercept_value.intercept)
 
 
-# %% [markdown]
-# You can run the workflow locally as follows:
-# %%
 if __name__ == "__main__":
     print(f"Running simple_wf_with_named_outputs() {simple_wf_with_named_outputs()}")
