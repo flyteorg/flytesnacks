360d58f5b8d2ae1d673597f547b3390eae0471cf
nikki everett <nikki@union.ai>
2024-02-22 17:40:41 -0600

    remove markdown from customizing dependencies files
    Signed-off-by: nikki everett <nikki@union.ai>

--- examples/customizing_dependencies/README.md
+++ examples/customizing_dependencies/README.md
@@ -1,13 +1,3 @@
-# Customizing Dependencies
+# Customizing dependencies
 
-In this section, you will uncover how Flyte utilizes Docker images to construct containers under the hood,
-and you'll learn how to craft your own images to encompass all the necessary dependencies for your tasks or workflows.
-You will explore how to execute a raw container with custom commands,
-indicate multiple container images within a single workflow,
-and get familiar with the ins and outs of `ImageSpec`!
-
-```{auto-examples-toc}
-image_spec
-raw_container
-multi_images
-```
+This directory contains runnable examples for the "[Customizing dependencies](https://docs.flyte.org/en/latest/user_guide/customizing_dependencies/index.html)" documentation.
--- examples/customizing_dependencies/customizing_dependencies/raw_container.py
+++ examples/customizing_dependencies/customizing_dependencies/raw_container.py
@@ -1,37 +1,10 @@
-# %% [markdown]
-# (raw_container)=
-#
-# # Using Raw Containers
-#
-# ```{eval-rst}
-# .. tags:: Containerization, Advanced
-# ```
-#
-# This example demonstrates how to use arbitrary containers in 5 different languages, all orchestrated in flytekit seamlessly.
-# Flyte mounts an input data volume where all the data needed by the container is available, and an output data volume
-# for the container to write all the data which will be stored away.
-#
-# The data is written as separate files, one per input variable. The format of the file is serialized strings.
-# Refer to the raw protocol to understand how to leverage this.
-# %%
 import logging
 
 from flytekit import ContainerTask, kwtypes, task, workflow
 
 logger = logging.getLogger(__file__)
 
-# %% [markdown]
-# ## Container Tasks
-#
-# A {py:class}`flytekit.ContainerTask` denotes an arbitrary container. In the following example, the name of the task
-# is `calculate_ellipse_area_shell`. This name has to be unique in the entire project. Users can specify:
-#
-# - `input_data_dir` -> where inputs will be written to.
-# - `output_data_dir` -> where Flyte will expect the outputs to exist.
-#
-# `inputs` and `outputs` specify the interface for the task; thus it should be an ordered dictionary of typed input and
-# output variables.
-# %%
+
 calculate_ellipse_area_shell = ContainerTask(
     name="ellipse-area-metadata-shell",
     input_data_dir="/var/inputs",
@@ -132,10 +105,6 @@ def report_all_calculated_areas(
     logger.info(f"julia: area={area_julia}, metadata={metadata_julia}")
 
 
-# %% [markdown]
-# As can be seen in this example, `ContainerTask`s can be interacted with like normal Python functions, whose inputs
-# correspond to the declared input variables. All data returned by the tasks are consumed and logged by a Flyte task.
-# %%
 @workflow
 def wf(a: float, b: float):
     # Calculate area in all languages
@@ -158,46 +127,3 @@ def wf(a: float, b: float):
         area_julia=area_julia,
         metadata_julia=metadata_julia,
     )
-
-
-# %% [markdown]
-# One of the benefits of raw container tasks is that Flytekit does not need to be installed in the target container.
-#
-# :::{note}
-# Raw containers cannot be run locally at the moment.
-# :::
-#
-# ## Scripts
-#
-# The contents of each script specified in the `ContainerTask` is as follows:
-#
-# ### calculate-ellipse-area.sh
-#
-# ```{literalinclude} ../../../examples/customizing_dependencies/raw-containers-supporting-files/per-language/shell/calculate-ellipse-area.sh
-# :language: shell
-# ```
-#
-# ### calculate-ellipse-area.py
-#
-# ```{literalinclude} ../../../examples/customizing_dependencies/raw-containers-supporting-files/per-language/python/calculate-ellipse-area.py
-# :language: python
-# ```
-#
-# ### calculate-ellipse-area.R
-#
-# ```{literalinclude} ../../../examples/customizing_dependencies/raw-containers-supporting-files/per-language/r/calculate-ellipse-area.R
-# :language: r
-# ```
-#
-# ### calculate-ellipse-area.hs
-#
-# ```{literalinclude} ../../../examples/customizing_dependencies/raw-containers-supporting-files/per-language/haskell/calculate-ellipse-area.hs
-# :language: haskell
-# ```
-#
-# ### calculate-ellipse-area.jl
-#
-# ```{literalinclude} ../../../examples/customizing_dependencies/raw-containers-supporting-files/per-language/julia/calculate-ellipse-area.jl
-# :language: julia
-# ```
-#
