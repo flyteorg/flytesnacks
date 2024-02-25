97aad15ba7169bd9e9788d16c224c5d1429048a8
Kevin Su <pingsutw@gmail.com>
2024-02-25 02:15:21 -0800

    Fix CI errors
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

---
+++ examples/airflow_agent/airflow_agent/airflow_agent.py
@@ -0,0 +1,23 @@
+# %% [markdown]
+# (airflow_agent)=
+# # Airflow agent example
+#
+# %%
+
+from airflow.sensors.filesystem import FileSensor
+from flytekit import task, workflow
+
+
+@task()
+def t1():
+    print("flyte")
+
+
+@workflow
+def wf():
+    sensor = FileSensor(task_id="id", filepath="/tmp/1234")
+    sensor >> t1()
+
+
+if __name__ == "__main__":
+    wf()
