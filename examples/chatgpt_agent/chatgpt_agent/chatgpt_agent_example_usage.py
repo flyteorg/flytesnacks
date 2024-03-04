e2b15e7c7a350960154b4781ac71b7bfd029f575
Future-Outlier <eric901201@gmail.com>
2024-03-04 21:50:01 +0800

    fix grammar error
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- examples/chatgpt_agent/chatgpt_agent/chatgpt_agent_example_usage.py
+++ examples/chatgpt_agent/chatgpt_agent/chatgpt_agent_example_usage.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 # ## ChatGPT Summary Bot
 # These examples show you the real use case of ChatGPT in the production mode.
 #
-# For more details, you can refer the [github repository](https://github.com/Future-Outlier/FlyteChatGPTSummaryBot) and [demo video](https://youtu.be/IS6gi4jR7h0?si=hWHZp5LyjDspiwfD).
+# For more details, you can refer the [github repository](https://github.com/Future-Outlier/FlyteChatGPTSummaryBot) and the [demo video](https://youtu.be/IS6gi4jR7h0?si=hWHZp5LyjDspiwfD).
 # ### Summarize Flyte's Latest Release from GitHub to Slack
 #
 # %%
