10c49c63b2d88234631c484b233f627e720429ab
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-03-11 10:20:43 -0700

    array node retries
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- examples/mems/map_retries.py
+++ examples/mems/map_retries.py
@@ -1,4 +1,5 @@
 import os
+from random import randint
 from collections import defaultdict
 from flytekit import task, workflow
 from flytekit.experimental import map_task
@@ -7,16 +8,20 @@ from flytekit.experimental import map_task
 NUM_ATTEMPTS = defaultdict(int)
 
 
-@task(retries=1)
+@task(retries=2)
 def flakey_map_task(*, task_id: int) -> None:
-    print(f"Envs {os.environ}")
-    NUM_ATTEMPTS[task_id] += 1
-    if NUM_ATTEMPTS[task_id] == 1:
+    for x, y in os.environ.items():
+        print(f"{x}: {y}")
+
+    r = randint(0, 10)
+    print(f"Random number: {r}")
+    if r < 5:
+        print("Failing")
         raise ValueError("Bad luck, this one failed")
 
 
 @workflow
 def flakey_map_workflow() -> None:
-    task_ids = list(range(2))
+    task_ids = list(range(3))
     map_task(flakey_map_task)(task_id=task_ids)
 
