28430fe81eb9377fc804ca0eb987ca3ace344668
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-03-11 14:58:23 -0700

    use attempt number
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- examples/mems/map_retries.py
+++ examples/mems/map_retries.py
@@ -3,25 +3,24 @@ from random import randint
 from collections import defaultdict
 from flytekit import task, workflow
 from flytekit.experimental import map_task
-
+from flytekit.exceptions.base import FlyteRecoverableException
 
 NUM_ATTEMPTS = defaultdict(int)
 
 
 @task(retries=2)
 def flakey_map_task(*, task_id: int) -> None:
-    for x, y in os.environ.items():
-        print(f"{x}: {y}")
+    attempt = os.environ["FLYTE_ATTEMPT_NUMBER"]
+    print(f"Task attempt: {attempt}")
 
     r = randint(0, 10)
-    print(f"Random number: {r}")
+    print(f"Random number is: {r}")
     if r < 5:
-        print("Failing")
-        raise ValueError("Bad luck, this one failed")
+        print("Failing task")
+        raise FlyteRecoverableException(f"Bad luck, this one failed {task_id}")
 
 
 @workflow
 def flakey_map_workflow() -> None:
     task_ids = list(range(3))
     map_task(flakey_map_task)(task_id=task_ids)
-
