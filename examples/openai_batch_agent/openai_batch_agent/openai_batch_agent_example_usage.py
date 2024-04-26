ef941a4fea33f82baff4c9fd8db987599f2cb76c
Samhita Alla <aallasamhita@gmail.com>
2024-04-26 12:32:51 +0530

    nit
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- examples/openai_batch_agent/openai_batch_agent/openai_batch_agent_example_usage.py
+++ examples/openai_batch_agent/openai_batch_agent/openai_batch_agent_example_usage.py
@@ -4,7 +4,7 @@
 # # Batching Requests for Asynchronous Processing
 #
 # This example demonstrates how to send a batch of API requests to GPT models for asynchronous processing.
-# 
+#
 # Every batch input should include `custom_id`, `method`, `url`, and `body`.
 # You can provide either a `JSONLFile` or `Iterator[JSON]`, and the agent handles the file upload to OpenAI,
 # creation of the batch, and downloading of the output and error files.
@@ -18,7 +18,7 @@ from typing import Iterator
 from flytekit import workflow
 from flytekit.types.file import JSONLFile
 from flytekit.types.iterator import JSON
-from flytekitplugins.openai import BatchResult, create_batch 
+from flytekitplugins.openai import BatchResult, create_batch
 
 
 def jsons():
@@ -61,14 +61,15 @@ batch = create_batch(
 def json_iterator_wf(json_vals: Iterator[JSON] = jsons()) -> BatchResult:
     return batch(jsonl_in=json_vals)
 
+
 # %% [markdown]
 # The `create_batch` function returns an imperative workflow responsible for uploading the JSON data to OpenAI,
-# creating a batch, polling the status of the batch to check for completion, and downloading the 
-# output and error files. It also accepts a `config` parameter, allowing you to provide `metadata`, `endpoint`, 
+# creating a batch, polling the status of the batch to check for completion, and downloading the
+# output and error files. It also accepts a `config` parameter, allowing you to provide `metadata`, `endpoint`,
 # and `completion_window` values. These parameters default to their respective default values.
 #
 # `BatchResult` is a `NamedTuple` that contains the paths to the output file and the error file.
-# 
+#
 # ## Using `JSONLFile`
 #
 # The following code snippet demonstrates how to send a JSONL file to the `create_batch` function:
@@ -77,10 +78,10 @@ def json_iterator_wf(json_vals: Iterator[JSON] = jsons()) -> BatchResult:
 def jsonl_wf() -> BatchResult:
     return batch(jsonl_in=JSONLFile("data.jsonl"))
 
+
 # %% [markdown]
 # The iterator streams JSONs to reduce the memory footprint. If you have large batches of requests that are huge in size,
-# you can leverage the iterator. It streams the data to a JSONL file and uploads it to OpenAI, whereas `JSONLFile` 
+# you can leverage the iterator. It streams the data to a JSONL file and uploads it to OpenAI, whereas `JSONLFile`
 # downloads the file in its entirety at once and uploads it to OpenAI.
 #
 # You can find more info about the [Batch API in the OpenAI docs](https://help.openai.com/en/articles/9197833-batch-api-faq).
-# %%
