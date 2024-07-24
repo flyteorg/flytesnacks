d091fbd3cf61d30899fdcf685b11f2b4251f7b67
Kevin Su <pingsutw@apache.org>
2024-07-24 11:43:59 +0800

    Fix pandera plugin examples (#1711)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/pandera_plugin/pandera_plugin/validating_and_testing_ml_pipelines.py
+++ examples/pandera_plugin/pandera_plugin/validating_and_testing_ml_pipelines.py
@@ -52,7 +52,7 @@ from pandera.typing import DataFrame, Index, Series  # noqa: F401
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-custom_image = ImageSpec(registry="ghcr.io/flyteorg", packages=["flytekitplugins-pandera"])
+custom_image = ImageSpec(registry="ghcr.io/flyteorg", packages=["flytekitplugins-pandera", "scikit-learn"])
 
 # %% [markdown]
 # We also need to import the `pandera` flytekit plugin to enable dataframe runtime type-checking:
