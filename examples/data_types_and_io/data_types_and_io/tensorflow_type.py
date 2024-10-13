44f079d751e01f90cfca7c1c2dcb3194af7ae60a
sumana sree <sumanasree2705@gmail.com>
2024-10-13 12:16:16 +0530

    Apply lint corrections using pre-commit hooks
    Signed-off-by: sumana sree <sumanasree2705@gmail.com>

--- examples/data_types_and_io/data_types_and_io/tensorflow_type.py
+++ examples/data_types_and_io/data_types_and_io/tensorflow_type.py
@@ -1,61 +1,54 @@
 # Import necessary libraries and modules
+
 import tensorflow as tf
 from flytekit import task, workflow
-from flytekit.types.file import TFRecordFile
 from flytekit.types.directory import TFRecordsDirectory
-import os
+from flytekit.types.file import TFRecordFile
+
 
 # TensorFlow Model
 @task
 def train_model() -> tf.keras.Model:
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model = tf.keras.Sequential(
+        [tf.keras.layers.Dense(128, activation="relu"), tf.keras.layers.Dense(10, activation="softmax")]
+    )
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     return model
+
 
 @task
 def evaluate_model(model: tf.keras.Model, x: tf.Tensor, y: tf.Tensor) -> float:
     loss, accuracy = model.evaluate(x, y)
     return accuracy
 
+
 @workflow
 def training_workflow(x: tf.Tensor, y: tf.Tensor) -> float:
     model = train_model()
-    accuracy = evaluate_model(model=model, x=x, y=y)
-    return accuracy
+    return evaluate_model(model=model, x=x, y=y)
 
 
-# TensorFlow Record File
 @task
 def process_tfrecord(file: TFRecordFile) -> int:
-    dataset = tf.data.TFRecordDataset(file)
     count = 0
-    for raw_record in dataset:
-        example = tf.train.Example()
-        example.ParseFromString(raw_record.numpy())
+    for record in tf.data.TFRecordDataset(file):
         count += 1
     return count
+
 
 @workflow
 def tfrecord_workflow(file: TFRecordFile) -> int:
     return process_tfrecord(file=file)
 
 
-# TensorFlow Records Directory
 @task
 def process_tfrecords_dir(dir: TFRecordsDirectory) -> int:
-    files = [f.path for f in os.scandir(dir) if f.is_file()]
-    dataset = tf.data.TFRecordDataset(files)
     count = 0
-    for raw_record in dataset:
-        example = tf.train.Example()
-        example.ParseFromString(raw_record.numpy())
+    for record in tf.data.TFRecordDataset(dir.path):
         count += 1
     return count
+
 
 @workflow
 def tfrecords_dir_workflow(dir: TFRecordsDirectory) -> int:
     return process_tfrecords_dir(dir=dir)
-
