8888b3a62b24efcf727d359a67acc0009fb04342
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-03-17 23:48:16 +0800

    docs: Add DL model training wf
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- examples/slurm_agent/slurm_agent/slurm_agent_example_usage.py
+++ examples/slurm_agent/slurm_agent/slurm_agent_example_usage.py
@@ -389,3 +389,217 @@ def eval_epoch(
     acc = (y_true == y_pred).sum() / len(y_true)
 
     return eval_loss_avg, acc
+
+
+# %% [markdown]
+# ### Deep Learning Model Training Workflow
+# Once the three main components are in place, you can now train your deep learning model using GPUs on the Slurm cluster with the highly flexible `SlurmFunctionTask`.
+# %%
+import os
+from pathlib import Path
+from typing import Dict, Optional
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from flytekit import task, workflow
+from flytekit.types.file import FlyteFile
+from flytekitplugins.slurm import SlurmFunction
+from torch.utils.data import DataLoader
+
+
+@task(
+    task_config=SlurmFunction(
+        ssh_config={
+            "host": "ec2-11-22-33-444.us-west-2.compute.amazonaws.com",
+            "username": "ubuntu",
+        },
+        sbatch_conf={
+            "partition": "debug",
+            "job-name": "process-data",
+            "output": "/home/ubuntu/dp.log",
+        },
+        script="""#!/bin/bash -i
+
+echo "Process and build torch datasets..."
+{task.fn}
+""",
+    )
+)
+def process_data(raw_data_path: str) -> str:
+    # Download the MNIST dataset but ignore the torch training and validation datasets,
+    # which are built in the `train` function below
+    _ = get_dataset(download_path=raw_data_path)
+    proc_data_path = raw_data_path
+
+    return proc_data_path
+
+
+@task(
+    task_config=SlurmFunction(
+        ssh_config={
+            "host": "ec2-11-22-33-444.us-west-2.compute.amazonaws.com",
+            "username": "ubuntu",
+        },
+        sbatch_conf={
+            "partition": "debug",
+            "job-name": "train-model",
+            "output": "/home/ubuntu/train.log",
+        },
+        script="""#!/bin/bash -i
+
+echo "Training process..."
+{task.fn}
+""",
+    )
+)
+def train(
+    data_path: str,
+    epochs: int = 5,
+    batch_size: int = 32,
+    lr: float = 1e-3,
+    ckpt_path: Optional[str] = None,
+    debug: bool = False,
+) -> FlyteFile:
+    # --------------------------------------------------------------------------------
+    # HARD-CODE CUDA DEVICE AND ASSERT IT'S AVAILABLE
+    # --------------------------------------------------------------------------------
+    device = torch.device("cuda")
+    assert torch.cuda.is_available(), "Requested GPU but no CUDA device found!"
+    print(f"[train] Using device: {device}")
+
+    ckpt_path = Path("./output") if ckpt_path is None else Path(ckpt_path)
+    ckpt_path.mkdir(exist_ok=True)
+    model_path = ckpt_path / "model.pth"
+
+    # Build dataloaders
+    tr_ds, val_ds = get_dataset(download_path=data_path)
+    tr_loader = DataLoader(tr_ds, batch_size=batch_size, shuffle=True, drop_last=True)
+    val_loader = DataLoader(val_ds, batch_size=batch_size * 4, shuffle=False)
+
+    # Build model
+    model = Model()
+
+    # Builc loss criterion
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Build solvers
+    # Optimizer
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    # LR scheduler
+    # lr_skd = None
+
+    # Run training and evaluation
+    best_score = 1e16
+    for ep in range(epochs):
+        tr_loss = train_epoch(
+            tr_loader=tr_loader,
+            model=model,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            debug=debug,
+        )
+        val_loss, acc = eval_epoch(eval_loader=val_loader, model=model, loss_fn=loss_fn, debug=debug)
+
+        # Save model ckpt
+        if val_loss < best_score:
+            best_score = val_loss
+            torch.save(model.state_dict(), model_path)
+
+        print(f"Epoch [{ep+1}/{epochs}] TRAIN LOSS {tr_loss:.4f} | VAL LOSS {val_loss:.4f} | ACC {acc:.4f}")
+
+    return FlyteFile(path=model_path)
+
+
+@task(
+    task_config=SlurmFunction(
+        ssh_config={
+            "host": "ec2-11-22-33-444.us-west-2.compute.amazonaws.com",
+            "username": "ubuntu",
+        },
+        sbatch_conf={
+            "partition": "debug",
+            "job-name": "eval-model",
+            "output": "/home/ubuntu/eval.log",
+            "gres": "gpu:1",
+        },
+        script="""#!/bin/bash -i
+
+echo "Evaluation process..."
+{task.fn}
+""",
+    )
+)
+@torch.no_grad()
+def run_infer(data_path: str, model_path: FlyteFile) -> Dict[str, float]:
+    # --------------------------------------------------------------------------------
+    # HARD-CODE CUDA DEVICE AND ASSERT IT'S AVAILABLE
+    # --------------------------------------------------------------------------------
+    # Build validation dataloader
+    _, val_ds = get_dataset(download_path=data_path)
+    val_loader = DataLoader(val_ds, batch_size=2048, shuffle=False)
+
+    # Load model
+    model = Model()
+    model.load_state_dict(torch.load(model_path.download()))
+
+    y_true, y_pred = [], []
+    model.eval()
+    for i, batch_data in tqdm(enumerate(val_loader), total=len(val_loader)):
+        # Retrieve batched raw data
+        x, y = batch_data
+        inputs = {"x": x}
+
+        # Forward pass
+        logits = model(inputs)
+
+        # Record batched output
+        y_true.append(y.detach())
+        y_pred.append(logits.detach())
+
+    # Derive accuracy
+    y_true = torch.cat(y_true, dim=0)
+    y_pred = torch.cat(y_pred, dim=0).argmax(dim=1)
+    prf_report = {"acc": ((y_true == y_pred).sum() / len(y_true)).item()}
+
+    return prf_report
+
+
+@workflow
+def dl_wf(
+    raw_data_path: str,
+    epochs: int = 1,
+    debug: bool = True,
+) -> Dict[str, float]:
+    proc_data_path = process_data(raw_data_path=raw_data_path)
+    output_path = train(data_path=proc_data_path, epochs=epochs, debug=debug)
+    prf_report = run_infer(data_path=proc_data_path, model_path=output_path)
+
+    return prf_report
+
+
+# %% [markdown]
+# Run the following code snippet and enjoy your training journey!
+# %%
+if __name__ == "__main__":
+    from click.testing import CliRunner
+    from flytekit.clis.sdk_in_container import pyflyte
+
+    runner = CliRunner()
+    path = os.path.realpath(__file__)
+
+    # Local run
+    print(">>> LOCAL EXEC <<<")
+    result = runner.invoke(
+        pyflyte.main,
+        [
+            "run",
+            "--raw-output-data-prefix",
+            "s3://my-flyte-slurm-agent",
+            path,
+            "dl_wf",
+            "--raw_data_path",
+            "/tmp/torch_data",
+        ],
+    )
+    print(result.output)
